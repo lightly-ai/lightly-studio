75c5abd421ed91a841ee3a6ba79a79ccbbaacf58
IgorSusmelj <igor_7779@hotmail.com>
2025-10-25 17:58:13 +0200

    Use correct column expression

--- lightly_studio/src/lightly_studio/resolvers/dataset_resolver.py
+++ lightly_studio/src/lightly_studio/resolvers/dataset_resolver.py
@@ -79,7 +79,7 @@ def to_dataset_view(session: Session, dataset: DatasetTable) -> DatasetView:
     """Convert a DatasetTable to DatasetView with computed sample count."""
     sample_count = (
         session.exec(
-            select(func.count(SampleTable.sample_id)).where(
+            select(func.count('*')).where(
                 SampleTable.dataset_id == dataset.dataset_id
             )
         ).one()
