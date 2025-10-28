6d5f8950d4813dd2a093cc5d82301789233c4e57
LeonardoRosaa <leonardoorosaa16@gmail.com>
2025-10-28 14:39:45 +0100

    feat: updated the panel slider to be fancier

--- lightly_studio_view/src/routes/datasets/[dataset_id]/+layout.svelte
+++ lightly_studio_view/src/routes/datasets/[dataset_id]/+layout.svelte
@@ -41,6 +41,7 @@
     import PlotPanel from '$lib/components/PlotPanel/PlotPanel.svelte';
     import { Button } from '$lib/components/ui/index.js';
     import { PaneGroup, Pane, PaneResizer } from 'paneforge';
+    import { GripVertical } from '@lucide/svelte';
 
     const { data, children } = $props();
     const {
@@ -242,7 +243,7 @@
         <div class="flex min-h-0 flex-1 space-x-4 px-4">
             {#if isSamples || isAnnotations}
                 <div class="flex h-full min-h-0 w-80 flex-col">
-                    <div class="flex min-h-0 flex-1 flex-col rounded-[1vw] bg-card py-4">
+                    <div class="bg-card flex min-h-0 flex-1 flex-col rounded-[1vw] py-4">
                         <div
                             class="min-h-0 flex-1 space-y-2 overflow-y-auto px-4 pb-2 dark:[color-scheme:dark]"
                         >
@@ -270,13 +271,13 @@
                 <!-- When plot is shown, use PaneGroup for the main content + plot -->
                 <PaneGroup direction="horizontal" class="flex-1">
                     <Pane defaultSize={50} minSize={30} class="flex">
-                        <div class="flex flex-1 flex-col space-y-4 rounded-[1vw] bg-card p-4">
+                        <div class="bg-card flex flex-1 flex-col space-y-4 rounded-[1vw] p-4">
                             <div class="my-2 flex items-center space-x-4">
                                 <div class="flex-1">
                                     {#if hasEmbeddingSearch}
                                         <div class="relative">
                                             <Search
-                                                class="absolute left-2 top-[50%] h-4 w-4 translate-y-[-50%] text-muted-foreground"
+                                                class="text-muted-foreground absolute left-2 top-[50%] h-4 w-4 translate-y-[-50%]"
                                             />
                                             <Input
                                                 placeholder="Search images by description"
@@ -304,7 +305,7 @@
                                     </Button>
                                 {/if}
                             </div>
-                            <Separator class="mb-4 bg-border-hard" />
+                            <Separator class="bg-border-hard mb-4" />
                             <div class="flex min-h-0 flex-1 overflow-hidden">
                                 {@render children()}
                             </div>
@@ -312,8 +313,14 @@
                     </Pane>
 
                     <PaneResizer
-                        class="mx-2 w-1 cursor-col-resize bg-border transition-colors hover:bg-accent"
-                    />
+                        class="bg-border hover:bg-accent bg-background relative mx-2 flex w-1 cursor-col-resize items-center justify-center transition-colors"
+                    >
+                        <div
+                            class="bg-brand z-10 flex h-7 min-w-5 items-center justify-center "
+                        >
+                            <GripVertical />
+                        </div>
+                    </PaneResizer>
 
                     <Pane defaultSize={50} minSize={30} class="flex flex-col">
                         <PlotPanel />
@@ -321,7 +328,7 @@
                 </PaneGroup>
             {:else}
                 <!-- When plot is hidden or not samples view, show normal layout -->
-                <div class="flex flex-1 flex-col space-y-4 rounded-[1vw] bg-card p-4 pb-2">
+                <div class="bg-card flex flex-1 flex-col space-y-4 rounded-[1vw] p-4 pb-2">
                     {#if isSamples || isAnnotations}
                         <div class="my-2 flex items-center space-x-4">
                             <div class="flex-1">
@@ -329,7 +336,7 @@
                                 {#if isSamples && hasEmbeddingSearch}
                                     <div class="relative">
                                         <Search
-                                            class="absolute left-2 top-[50%] h-4 w-4 translate-y-[-50%] text-muted-foreground"
+                                            class="text-muted-foreground absolute left-2 top-[50%] h-4 w-4 translate-y-[-50%]"
                                         />
                                         <Input
                                             placeholder="Search images by description"
@@ -357,7 +364,7 @@
                                 </Button>
                             {/if}
                         </div>
-                        <Separator class="mb-4 bg-border-hard" />
+                        <Separator class="bg-border-hard mb-4" />
                     {/if}
 
                     <div class="flex min-h-0 flex-1">
