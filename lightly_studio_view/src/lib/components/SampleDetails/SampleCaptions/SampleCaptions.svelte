a853b12799e4a3cb0d9698795549452849bd363b
LeonardoRosaa <leonardoorosaa16@gmail.com>
2025-10-30 15:21:33 +0100

    feat(captions): display captions within Sample Details

---
+++ lightly_studio_view/src/lib/components/SampleDetails/SampleCaptions/SampleCaptions.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import { Segment } from '$lib/components';
+
+    const { captions }: { captions: { text: string }[] } = $props();
+</script>
+
+<Segment title="Captions">
+    <div
+        class="text-diffuse-foreground flex flex-col items-start justify-start gap-y-3"
+    >
+        {#each captions as { text }}
+            <span class="text-sm">{text}</span>
+        {/each}
+    </div>
+</Segment>
