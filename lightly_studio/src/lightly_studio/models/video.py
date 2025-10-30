640a93f05344cbb406243153d1f7674bc50b2ffd
JonasWurst <jonas@lightly.ai>
2025-10-30 13:17:09 +0100

    Review changes

--- lightly_studio/src/lightly_studio/models/video.py
+++ lightly_studio/src/lightly_studio/models/video.py
@@ -1,10 +1,8 @@
 """This module defines the Video and VideoFrame model for the application."""
 
-from typing import TYPE_CHECKING, List, Optional
+from typing import TYPE_CHECKING, List
 from uuid import UUID
 
-from pydantic import BaseModel, ConfigDict
-from pydantic import Field as PydanticField
 from sqlalchemy.orm import Mapped
 from sqlmodel import Field, Relationship, SQLModel
 
@@ -19,7 +17,53 @@ else:
     SampleTable = object
 
 
-# VideoFrame
+class VideoBase(SQLModel):
+    """Base class for the Video model."""
+
+    """The width of the video in pixels."""
+    width: int
+
+    """The height of the video in pixels."""
+    height: int
+
+    """The duration of the video in seconds."""
+    duration: float
+
+    """The fps of the video."""
+    fps: float
+
+    """The file name of the video."""
+    file_name: str
+
+    """The path of the video."""
+    file_path_abs: str = Field(default=None)
+
+
+class VideoCreate(VideoBase):
+    """Video class when inserting."""
+
+
+class VideoTable(VideoBase, table=True):
+    """This class defines the Video ORM table."""
+
+    __tablename__ = "video"
+    sample_id: UUID = Field(foreign_key="sample.sample_id", primary_key=True)
+    frames: Mapped[List["VideoFrameTable"]] = Relationship(back_populates="video")
+    sample: Mapped["SampleTable"] = Relationship()
+
+
+class VideoView(SQLModel):
+    """Video class when retrieving."""
+
+    width: int
+    height: int
+    duration: float
+    fps: float
+    file_name: str
+    file_path_abs: str
+    sample_id: UUID
+    sample: SampleView
+
 
 class VideoFrameBase(SQLModel):
     """Base class for the VideoFrame model."""
@@ -43,9 +87,6 @@ class VideoFrameTable(VideoFrameBase, table=True):
 
     __tablename__ = "video_frame"
     sample_id: UUID = Field(foreign_key="sample.sample_id", primary_key=True)
-    annotations: Mapped[List["AnnotationBaseTable"]] = Relationship(
-        back_populates="sample",
-    )
 
     sample: Mapped["SampleTable"] = Relationship()
     video: Mapped["VideoTable"] = Relationship(back_populates="frames")
@@ -54,97 +95,11 @@ class VideoFrameTable(VideoFrameBase, table=True):
 class VideoFrameView(SQLModel):
     """VideoFrame class when retrieving."""
 
-    class VideoFrameViewVideo(SQLModel):
-        """Video view inside VideoFrame view."""
-        file_name: str
-        file_path_abs: str
-        sample_id: UUID
-        width: int
-        height: int
-        duration: float
-        fps: float
-
-    """The frame number of the video frame."""
     frame_number: int
     frame_timestamp: int
     sample_id: UUID
     video_sample_id: UUID
 
     # Video metadata routed from parent video
-    video: VideoFrameViewVideo
+    video: VideoView
     sample: SampleView
-
-
-class VideoFrameViewsWithCount(BaseModel):
-    """Response model for counted video frames."""
-
-    model_config = ConfigDict(populate_by_name=True)
-
-    samples: List[VideoFrameView] = PydanticField(..., alias="data")
-    total_count: int
-    next_cursor: Optional[int] = PydanticField(None, alias="nextCursor")
-
-
-
-# Video
-
-class VideoBase(SQLModel):
-    """Base class for the Video model."""
-
-    """The width of the video in pixels."""
-    width: int
-
-    """The height of the video in pixels."""
-    height: int
-
-    """The duration of the video in seconds."""
-    duration: float
-
-    """The fps of the video."""
-    fps: float
-
-    """The file name of the video."""
-    file_name: str
-
-    """The path of the video."""
-    file_path_abs: str = Field(default=None)
-
-    """The dataset ID to which the video belongs."""
-    dataset_id: UUID = Field(default=None, foreign_key="dataset.dataset_id")
-
-
-class VideoCreate(VideoBase):
-    """Video class when inserting."""
-
-
-class VideoTable(VideoBase, table=True):
-    """This class defines the Video ORM table."""
-
-    __tablename__ = "video"
-    sample_id: UUID = Field(foreign_key="sample.sample_id", primary_key=True)
-    frames: Mapped[List["VideoFrameTable"]] = Relationship(back_populates="video")
-    sample: Mapped["SampleTable"] = Relationship()
-
-
-class VideoView(SQLModel):
-    """Video class when retrieving."""
-
-    file_name: str
-    file_path_abs: str
-    sample_id: UUID
-    sample: SampleView
-    frames: List["VideoFrameView"] = []
-    width: int
-    height: int
-    duration: float
-    fps: float
-
-
-class VideoViewsWithCount(BaseModel):
-    """Response model for counted videos."""
-
-    model_config = ConfigDict(populate_by_name=True)
-
-    samples: List[VideoView] = PydanticField(..., alias="data")
-    total_count: int
-    next_cursor: Optional[int] = PydanticField(None, alias="nextCursor")
