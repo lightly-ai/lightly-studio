3c011a909d5de013c90f778a17773dd882af2d24
Michal Szabados <michal@lightly.ai>
2025-10-30 11:25:32 +0100

    Split image resolver

---
+++ lightly_studio/src/lightly_studio/resolvers/image_resolver/get_by_id.py
@@ -0,0 +1,18 @@
+"""Implementation of get_by_id function for images."""
+
+from __future__ import annotations
+
+from uuid import UUID
+
+from sqlmodel import Session, select
+
+from lightly_studio.models.image import ImageTable
+
+
+def get_by_id(session: Session, dataset_id: UUID, sample_id: UUID) -> ImageTable | None:
+    """Retrieve a single sample by ID."""
+    return session.exec(
+        select(ImageTable).where(
+            ImageTable.sample_id == sample_id, ImageTable.dataset_id == dataset_id
+        )
+    ).one_or_none()
