a5ea1c5f56f42fa9b3d8a31b4868a4e94e23c2f1
michal-lightly <105644579+michal-lightly@users.noreply.github.com>
2025-10-30 17:21:21 +0100

    Split image resolver tests (#72)

---
+++ lightly_studio/tests/resolvers/image_resolver/test_get_many_by_id.py
@@ -0,0 +1,37 @@
+from sqlmodel import Session
+
+from lightly_studio.resolvers import (
+    image_resolver,
+)
+from tests.helpers_resolvers import (
+    create_dataset,
+    create_image,
+)
+
+
+def test_get_many_by_id(
+    test_db: Session,
+) -> None:
+    dataset = create_dataset(session=test_db)
+    dataset_id = dataset.dataset_id
+    # Create samples.
+    image1 = create_image(
+        session=test_db,
+        dataset_id=dataset_id,
+        file_path_abs="/path/to/sample1.png",
+    )
+    image2 = create_image(
+        session=test_db,
+        dataset_id=dataset_id,
+        file_path_abs="/path/to/sample2.png",
+    )
+
+    # Act.
+    samples = image_resolver.get_many_by_id(
+        session=test_db, sample_ids=[image1.sample_id, image2.sample_id]
+    )
+
+    # Assert.
+    assert len(samples) == 2
+    assert samples[0].file_name == "sample1.png"
+    assert samples[1].file_name == "sample2.png"
