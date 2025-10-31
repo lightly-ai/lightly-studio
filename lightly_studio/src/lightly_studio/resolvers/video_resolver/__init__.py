a73105d9c423965c9c532741eb8b28d4190b5066
JonasWurst <jonas@lightly.ai>
2025-10-31 10:10:14 +0100

    Remove unused methods from init

--- lightly_studio/src/lightly_studio/resolvers/video_resolver/__init__.py
+++ lightly_studio/src/lightly_studio/resolvers/video_resolver/__init__.py
@@ -4,9 +4,7 @@ from lightly_studio.resolvers.video_resolver.create import create, create_many
 from lightly_studio.resolvers.video_resolver.get_all_by_dataset_id import get_all_by_dataset_id
 
 __all__ = [
-    "count_by_dataset_id",
     "create",
     "create_many",
-    "filter_new_paths",
     "get_all_by_dataset_id",
 ]
