247362d266904d2cd5dfee055c25ac98e168398f
Horatiu Almasan <horatiu@lightly.ai>
2025-11-05 12:54:46 +0200

    fix

--- lightly_studio/src/lightly_studio/core/logging.py
+++ lightly_studio/src/lightly_studio/core/logging.py
@@ -8,7 +8,7 @@ from uuid import UUID
 from sqlmodel import Session
 
 from lightly_studio.resolvers import (
-    image_resolver,
+    sample_resolver,
 )
 
 # Constants
@@ -35,7 +35,7 @@ class _LoadingLoggingContext:
 def _log_loading_results(
     session: Session, dataset_id: UUID, logging_context: _LoadingLoggingContext
 ) -> None:
-    n_samples_end = image_resolver.count_by_dataset_id(session=session, dataset_id=dataset_id)
+    n_samples_end = sample_resolver.count_by_dataset_id(session=session, dataset_id=dataset_id)
     n_samples_inserted = n_samples_end - logging_context.n_samples_before_loading
     print(
         f"Added {n_samples_inserted} out of {logging_context.n_samples_to_be_inserted}"
