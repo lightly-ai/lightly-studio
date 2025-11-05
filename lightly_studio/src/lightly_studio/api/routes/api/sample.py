978aa70ed17f37efc82e05a38bb7e48cff2383c9
Michal Szabados <michal@lightly.ai>
2025-11-05 09:21:27 +0000

    Format

--- lightly_studio/src/lightly_studio/api/routes/api/sample.py
+++ lightly_studio/src/lightly_studio/api/routes/api/sample.py
@@ -17,8 +17,6 @@ from lightly_studio.api.routes.api.validators import Paginated
 from lightly_studio.db_manager import SessionDep
 from lightly_studio.models.dataset import DatasetTable
 from lightly_studio.models.image import (
-    ImageCreate,
-    ImageTable,
     ImageView,
     ImageViewsWithCount,
 )
