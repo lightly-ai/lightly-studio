6ec57ae90eeb07e0efe75d57b91ae1a6bbfc5777
LeonardoRosaa <leonardoorosaa16@gmail.com>
2025-11-14 13:53:39 -0300

    removed unused import

--- lightly_studio_view/src/routes/+page.ts
+++ lightly_studio_view/src/routes/+page.ts
@@ -1,5 +1,4 @@
 import { routeHelpers } from '$lib/routes';
-import { readDatasets, readRootDataset } from '$lib/api/lightly_studio_local/sdk.gen';
 import { redirect } from '@sveltejs/kit';
 import { SampleType } from '$lib/api/lightly_studio_local';
 import { useRootDataset } from '$lib/hooks/useRootDataset/useRootDataset';
