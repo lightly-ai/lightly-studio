28f9e3f78b1618afd0d23fc294bd9ef69bfc8bcc
LeonardoRosaa <leonardoorosaa16@gmail.com>
2025-11-17 13:18:49 -0300

    fix: removed video frames from video details

--- lightly_studio_view/src/routes/datasets/[dataset_id]/videos/[sample_id]/+page.svelte
+++ lightly_studio_view/src/routes/datasets/[dataset_id]/videos/[sample_id]/+page.svelte
@@ -2,40 +2,53 @@
     import { Card, CardContent, Segment } from '$lib/components';
     import { PUBLIC_VIDEOS_MEDIA_URL, PUBLIC_VIDEOS_FRAMES_MEDIA_URL } from '$env/static/public';
     import type { PageData } from './$types';
-    import type { VideoView } from '$lib/api/lightly_studio_local';
+    import type { FrameView, VideoView } from '$lib/api/lightly_studio_local';
+    import { Button } from '$lib/components/ui';
 
     const { data }: { data: PageData } = $props();
     const { sample }: { sample: VideoView } = $derived(data);
+
+    let videoEl: HTMLVideoElement | null = null;
+    let currentFrame: FrameView | null = $state(onTimeUpdate());
+
+    function onTimeUpdate(): FrameView | null {
+        if (!sample.frames || sample.frames.length === 0 || !videoEl) return null;
+
+        const currentTime = videoEl.currentTime;
+
+        const pastFrames = sample.frames.filter((f) => f.frame_timestamp_s <= currentTime);
+
+        const frame =
+            pastFrames.length > 0
+                ? pastFrames.reduce((prev, curr) =>
+                      curr.frame_timestamp_s > prev.frame_timestamp_s ? curr : prev
+                  )
+                : sample.frames[0];
+
+        currentFrame = frame;
+
+        return frame;
+    }
 </script>
 
 <div class="flex h-full w-full flex-row gap-4 overflow-hidden p-4">
     <Card className="flex flex-col w-[60vw]">
         <CardContent className="flex flex-col gap-4 overflow-hidden h-full">
             <video
-                class="max-h-[80%] min-h-[80%] w-full rounded-lg object-contain shadow-md"
+                class="max-h-[100%] min-h-[100%] w-full rounded-lg object-contain shadow-md"
                 src={`${PUBLIC_VIDEOS_MEDIA_URL}/${sample.sample_id}`}
                 muted
                 controls
+                bind:this={videoEl}
+                ontimeupdate={onTimeUpdate}
             ></video>
-
-            <div class="no-scrollbar max-h-[20%] overflow-x-auto overflow-y-hidden">
-                <div class="flex min-w-max gap-2 px-1 pb-2">
-                    {#each sample.frames as frame}
-                        <img
-                            src={`${PUBLIC_VIDEOS_FRAMES_MEDIA_URL}/${frame.sample_id}`}
-                            alt="Frame"
-                            class="h-20 w-28 shrink-0 cursor-pointer rounded-lg object-cover shadow-sm transition-transform duration-150 hover:scale-105"
-                        />
-                    {/each}
-                </div>
-            </div>
         </CardContent>
     </Card>
 
     <Card className="flex flex-col flex-1 overflow-hidden">
         <CardContent className="h-full overflow-y-auto">
             <Segment title="Sample details">
-                <div class="min-w-full space-y-3 text-diffuse-foreground">
+                <div class="text-diffuse-foreground min-w-full space-y-3">
                     <div class="flex items-start gap-3">
                         <span class="truncate text-sm font-medium" title="Width">Width:</span>
                         <span class="text-sm">{sample.width}px</span>
@@ -53,6 +66,22 @@
                         <span class="text-sm">{sample.fps.toFixed(2)}</span>
                     </div>
                 </div>
+            </Segment>
+            <Segment title="Current Frame">
+                {#if currentFrame}
+                    <div class="text-diffuse-foreground space-y-2 text-sm">
+                        <div class="flex items-center gap-2">
+                            <span class="font-medium">Frame #:</span>
+                            <span>{currentFrame.frame_number}</span>
+                        </div>
+                        <div class="flex items-center gap-2">
+                            <span class="font-medium">Timestamp:</span>
+                            <span>{currentFrame.frame_timestamp_s.toFixed(3)} s</span>
+                        </div>
+                    </div>
+                {/if}
+
+                <Button variant="secondary" class="mt-4 w-full" href={'/'}>View frame</Button>
             </Segment>
         </CardContent>
     </Card>
