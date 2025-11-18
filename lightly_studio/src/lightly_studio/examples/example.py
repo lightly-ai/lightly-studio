67c3ba0b29f682cf6d36a2941bae85fcca8cdc80
Michal Szabados <michal@lightly.ai>
2025-11-18 16:52:04 +0100

    Use separate environment variables for test scripts

--- lightly_studio/src/lightly_studio/examples/example.py
+++ lightly_studio/src/lightly_studio/examples/example.py
@@ -15,8 +15,7 @@ env.read_env()
 db_manager.connect(cleanup_existing=True)
 
 # Define the path to the dataset directory
-dataset_path = Path(env.path("DATASET_PATH", "/path/to/your/dataset"))
-dataset_path = dataset_path.parent if dataset_path.is_file() else dataset_path
+dataset_path = Path(env.path("EXAMPLES_DATASET_PATH", "/path/to/your/dataset"))
 
 # Create a Dataset from a path
 dataset = ls.Dataset.create()
