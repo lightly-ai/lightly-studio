873040f830e597a4c5ae56b138fa2c0e853d59d1
Kondrat Shmoylov <kondrat.shmoylov@gmail.com>
2025-11-24 09:37:49 +0100

    use dataset_examples

--- lightly_studio/e2e-tests/index_with_video.py
+++ lightly_studio/e2e-tests/index_with_video.py
@@ -20,6 +20,6 @@ current_dir = Path(__file__).resolve().parent
 
 # Create a Dataset from a path
 dataset = ls.Dataset.create(sample_type=ls.SampleType.VIDEO)
-dataset.add_videos_from_path(path="datasets/small_videos")
+dataset.add_videos_from_path(path="dataset_examples/youtube_vis_50_videos")
 
 ls.start_gui()
