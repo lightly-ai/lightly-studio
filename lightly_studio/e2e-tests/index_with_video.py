2a2c5ebcf075ef1cc0d179ed30afeee43b6c8e55
Kondrat Shmoylov <kondrat.shmoylov@gmail.com>
2025-11-24 10:02:40 +0100

    update

--- lightly_studio/e2e-tests/index_with_video.py
+++ lightly_studio/e2e-tests/index_with_video.py
@@ -1,12 +1,10 @@
 """End-to-end demonstration of the lightly_studio indexing video."""
 
-# We import the DatasetLoader class from the lightly_purple module
 import lightly_studio as ls
 
 # Clean up an existing database
 ls.db_manager.connect(cleanup_existing=True)
 
-# Create a DatasetLoader instance
 dataset = ls.Dataset.create(sample_type=ls.SampleType.VIDEO)
 dataset.add_videos_from_path(path="dataset_examples/youtube_vis_50_videos")
 
