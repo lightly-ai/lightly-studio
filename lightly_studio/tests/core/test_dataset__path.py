9a74d8c676357033d93e5c21149fd4d110a3c513
masakljun <masika.kljun@gmail.com>
2025-11-26 16:20:36 +0100

    small fix

--- lightly_studio/tests/core/test_dataset__path.py
+++ lightly_studio/tests/core/test_dataset__path.py
@@ -173,10 +173,6 @@ class TestDataset:
         dataset.add_images_from_path(path=images_path)
         assert len(list(dataset)) == 6
 
-        # log_text = caplog.text
-        # assert "Added 2 out of 6 new samples to the dataset." in log_text
-        # assert f"Examples of paths that were not added:  {images_path}" in log_text
-
         log_text = caplog.text
         assert "Added 2 out of 6 new samples to the dataset." in log_text
         assert "Examples of paths that were not added:" in log_text
