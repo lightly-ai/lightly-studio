2a3f6c5f770905746076461f84b61f09c28ba34e
Michal Szabados <michal@lightly.ai>
2025-11-28 16:58:20 +0100

    Test success

--- lightly_studio/src/lightly_studio/__init__.py
+++ lightly_studio/src/lightly_studio/__init__.py
@@ -10,5 +10,3 @@ from lightly_studio.models.dataset import SampleType
 from lightly_studio.models.annotation.annotation_base import AnnotationType
 
 __all__ = ["AnnotationType", "Dataset", "SampleType", "start_gui"]
-
-# Bust cache 3
