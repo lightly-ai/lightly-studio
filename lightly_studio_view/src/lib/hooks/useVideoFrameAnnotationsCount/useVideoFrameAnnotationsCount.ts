e77b97fbb077a5070e0e6ac4bb68ac517f63df07
Leonardo Rosa <43169851+LeonardoRosaa@users.noreply.github.com>
2025-12-03 10:04:25 -0300

    Add video frame annotations filtering to GUI (#244)

---
+++ lightly_studio_view/src/lib/hooks/useVideoFrameAnnotationsCount/useVideoFrameAnnotationsCount.ts
@@ -0,0 +1,19 @@
+import type { VideoFrameAnnotationsCounterFilter } from '$lib/api/lightly_studio_local';
+import { countVideoFrameAnnotationsByVideoDatasetOptions } from '$lib/api/lightly_studio_local/@tanstack/svelte-query.gen';
+import { createQuery } from '@tanstack/svelte-query';
+
+export const useVideoFrameAnnotationCounts = ({
+    datasetId,
+    filter
+}: {
+    datasetId: string;
+    filter: VideoFrameAnnotationsCounterFilter;
+}) =>
+    createQuery(
+        countVideoFrameAnnotationsByVideoDatasetOptions({
+            path: { video_frame_dataset_id: datasetId },
+            body: {
+                filter
+            }
+        })
+    );
