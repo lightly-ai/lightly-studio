2bf0f08f8af935000091991022bdd55fc9936cbf
Michal Szabados <michal@lightly.ai>
2025-12-04 16:51:22 +0100

    Update test

--- lightly_studio_view/e2e/general/caption.e2e-test.ts
+++ lightly_studio_view/e2e/general/caption.e2e-test.ts
@@ -20,9 +20,7 @@ test.describe('caption-flow1', () => {
 
         // Check the first caption
         expect(await captionUtils.getCaptionCount()).toEqual(1);
-        expect(await captionUtils.getNthCaption(0).getByTestId('caption-input')).toHaveValue(
-            'caption'
-        );
+        expect(await captionUtils.getNthCaptionInput(0)).toEqual('caption');
 
         // Add a second caption
         await captionUtils.addCaption();
@@ -30,9 +28,7 @@ test.describe('caption-flow1', () => {
 
         // Check the second caption
         expect(await captionUtils.getCaptionCount()).toEqual(2);
-        expect(await captionUtils.getNthCaption(1).getByTestId('caption-input')).toHaveValue(
-            'another caption'
-        );
+        expect(await captionUtils.getNthCaptionInput(1)).toEqual('another caption');
     });
 
     test('Delete a caption in sample details', async ({
@@ -50,21 +46,16 @@ test.describe('caption-flow1', () => {
 
         // Check the remaining caption
         expect(await captionUtils.getCaptionCount()).toEqual(1);
-        expect(await captionUtils.getNthCaption(0).getByTestId('caption-input')).toHaveValue(
-            'another caption'
-        );
+        expect(await captionUtils.getNthCaptionInput(0)).toEqual('another caption');
     });
 
     test('Add and edit captions in caption page', async ({ captionsPage, captionUtils }) => {
-        // Navigate to caption page
-        // await captionsPage.goto();
-
         // There should be just one sample
         expect(await captionsPage.getGridItemCount()).toEqual(1);
 
         // There should be just one caption
         expect(await captionUtils.getCaptionCount()).toEqual(1);
-        expect(await captionUtils.getNthCaption(0).textContent()).toEqual('another caption');
+        expect(await captionUtils.getNthCaptionText(0)).toEqual('another caption');
 
         // Toggle the edit mode
         await captionsPage.clickEditButton();
@@ -74,9 +65,7 @@ test.describe('caption-flow1', () => {
         await captionUtils.updateNthCaption(1, 'caption in caption page');
         expect(await captionsPage.getGridItemCount()).toEqual(1);
         expect(await captionUtils.getCaptionCount()).toEqual(2);
-        expect(await captionUtils.getNthCaption(1).getByTestId('caption-input')).toHaveValue(
-            'caption in caption page'
-        );
+        expect(await captionUtils.getNthCaptionInput(1)).toEqual('caption in caption page');
     });
 
     test('Delete a caption in caption page', async ({ captionsPage, captionUtils }) => {
@@ -86,9 +75,7 @@ test.describe('caption-flow1', () => {
 
         // Check the remaining caption
         expect(await captionUtils.getCaptionCount()).toEqual(1);
-        expect(await captionUtils.getNthCaption(0).getByTestId('caption-input')).toHaveValue(
-            'another caption'
-        );
+        expect(await captionUtils.getNthCaptionInput(0)).toEqual('another caption');
 
         // Deleting the last caption also hides the sample
         await captionUtils.deleteNthCaption(0);
