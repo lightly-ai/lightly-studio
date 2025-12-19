a7df08c2b62227a71ca0065a7698b843a7b37dd1
Michal Szabados <michal@lightly.ai>
2025-12-19 14:02:53 +0100

    Remove legacy label functions

--- lightly_studio/src/lightly_studio/api/routes/api/annotation_label.py
+++ lightly_studio/src/lightly_studio/api/routes/api/annotation_label.py
@@ -47,7 +47,7 @@ def create_annotation_label(
 def read_annotation_labels(
     session: SessionDep,
     # TODO(Michal, 12/2025): Pass the root dataset directly.
-    dataset_id: Annotated[  # noqa: ARG001
+    dataset_id: Annotated[
         UUID,
         Path(
             title="Dataset Id",
@@ -56,7 +56,9 @@ def read_annotation_labels(
     ],
 ) -> list[AnnotationLabelTable]:
     """Retrieve a list of annotation labels from the database."""
-    root_dataset_id = dataset_resolver.get_root_dataset(session=session, dataset_id=dataset_id).dataset_id
+    root_dataset_id = dataset_resolver.get_root_dataset(
+        session=session, dataset_id=dataset_id
+    ).dataset_id
     return annotation_label_resolver.get_all(session=session, root_dataset_id=root_dataset_id)
 
 
--- lightly_studio/src/lightly_studio/resolvers/annotation_label_resolver/__init__.py
+++ lightly_studio/src/lightly_studio/resolvers/annotation_label_resolver/__init__.py
@@ -4,13 +4,11 @@ from .create import create
 from .delete import delete
 from .get_all import (
     get_all,
-    get_all_legacy,
     get_all_sorted_alphabetically,
-    get_all_sorted_alphabetically_legacy,
 )
 from .get_by_id import get_by_id
 from .get_by_ids import get_by_ids
-from .get_by_label_name import get_by_label_name, get_by_label_name_legacy
+from .get_by_label_name import get_by_label_name
 from .names_by_ids import names_by_ids
 from .update import update
 
@@ -18,13 +16,10 @@ __all__ = [
     "create",
     "delete",
     "get_all",
-    "get_all_legacy",
     "get_all_sorted_alphabetically",
-    "get_all_sorted_alphabetically_legacy",
     "get_by_id",
     "get_by_ids",
     "get_by_label_name",
-    "get_by_label_name_legacy",
     "names_by_ids",
     "update",
 ]
