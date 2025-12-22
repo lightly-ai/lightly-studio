f3cf1d9baafb656ba706dc3fc48d2fbe4c3a7409
Michal Szabados <michal@lightly.ai>
2025-12-22 12:47:21 +0100

    Remove a todo

--- lightly_studio/src/lightly_studio/services/annotations_service/update_annotation_label.py
+++ lightly_studio/src/lightly_studio/services/annotations_service/update_annotation_label.py
@@ -36,8 +36,6 @@ def update_annotation_label(
 
     # Get root collection id from the annotation's current label
     dataset_id = annotation.annotation_label.dataset_id
-    # TODO(Michal, 12/2025): Remove the assertion once dataset_id is non-optional.
-    assert dataset_id is not None
 
     annotation_label = annotation_label_resolver.get_by_label_name(
         session=session,
