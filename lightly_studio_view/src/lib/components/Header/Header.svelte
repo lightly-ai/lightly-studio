8ef15773d9dac59fb4feb23da8ccfa5cb119c6ec
Michal Szabados <michal@lightly.ai>
2025-12-23 14:26:29 +0100

    FOrmat

--- lightly_studio_view/src/lib/components/Header/Header.svelte
+++ lightly_studio_view/src/lib/components/Header/Header.svelte
@@ -42,7 +42,7 @@
 <svelte:window onkeydown={handleKeyDown} />
 
 <header>
-    <div class="p border-border-hard bg-card text-diffuse-foreground mb-3 border-b px-4 py-4 pl-8">
+    <div class="p mb-3 border-b border-border-hard bg-card px-4 py-4 pl-8 text-diffuse-foreground">
         <div class="flex justify-between">
             <div class="flex w-[320px]">
                 <a href="/collections/{collection.collection_id}"><Logo /></a>
