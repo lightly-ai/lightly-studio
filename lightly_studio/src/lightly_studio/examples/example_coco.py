94a13451740a48bd992f9e3f713af7fb10c25d84
MalteEbner <malte.ebner@gmail.com>
2026-01-06 11:23:52 +0100

    Move coco example to jupyter notebook

--- lightly_studio/src/lightly_studio/examples/example_coco.py
+++ lightly_studio/src/lightly_studio/examples/example_coco.py
@@ -1,7 +1,5 @@
 """Example of how to add samples in coco format to a dataset."""
 
-import time
-
 from environs import Env
 
 import lightly_studio as ls
@@ -26,21 +24,4 @@ dataset.add_samples_from_coco(
     annotation_type=ls.AnnotationType.INSTANCE_SEGMENTATION,
 )
 
-ls.start_gui_background()
-print("Waiting for GUI to start...")
-ls.check_gui_background()
-print("GUI started.")
-
-time.sleep(20)
-
-print("Adding metadata to samples...")
-for i, sample in enumerate(dataset):
-    sample.metadata["index_in_dataset"] = i
-
-print("Metadata added.")
-
-time.sleep(20)
-
-print("Stopping GUI...")
-ls.stop_gui_background()
-print("GUI stopped.")
+ls.start_gui()
