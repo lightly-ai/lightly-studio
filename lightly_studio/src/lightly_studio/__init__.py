4add39bbde6ee7785d7db62d9320f28df3da7551
MalteEbner <malte.ebner@gmail.com>
2026-01-07 16:52:42 +0100

    undo wrong merge conflict solving

--- lightly_studio/src/lightly_studio/__init__.py
+++ lightly_studio/src/lightly_studio/__init__.py
@@ -1,14 +1,13 @@
 # Set up logging before importing any other modules.
 # Add noqa to silence unused import and unsorted imports linter warnings.
 from . import setup_logging  # noqa: F401 I001
-from lightly_studio.core.image_dataset import ImageDataset as Dataset
-from lightly_studio.core import start_gui as start_gui_module
 
 # Import db_manager for SQLModel to discover db models.
 from lightly_studio import db_manager  # noqa: F401
 
 from lightly_studio.core.image_dataset import ImageDataset
 from lightly_studio.core.video_dataset import VideoDataset
+from lightly_studio.core import start_gui as start_gui_module
 from lightly_studio.models.collection import SampleType
 
 
