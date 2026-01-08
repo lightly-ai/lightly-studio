9e28c7a72b37b527395818fbc8af068752c9cc1a
LeonardoRosaa <leonardoorosaa16@gmail.com>
2026-01-08 17:42:36 -0300

    move instance segmentation creation to toolbar

--- lightly_studio_view/src/lib/components/SampleDetails/SampleDetailsToolbar/SampleDetailsToolbar.svelte
+++ lightly_studio_view/src/lib/components/SampleDetails/SampleDetailsToolbar/SampleDetailsToolbar.svelte
@@ -2,6 +2,7 @@
     import { Card, CardContent } from '$lib/components';
     import SampleDetailsToolbarTooltip from '$lib/components/SampleDetails/SampleDetailsToolbarTooltip/SampleDetailsToolbarTooltip.svelte';
     import BoundingBoxToolbarButton from '../BoundingBoxToolbarButton/BoundingBoxToolbarButton.svelte';
+    import BrushToolbarButton from '../BrushToolbarButton/BrushToolbarButton.svelte';
 
     type SampleDetailsToolbar = {
         collectionId: string;
@@ -19,6 +20,9 @@
     <CardContent>
         <SampleDetailsToolbarTooltip label="Bounding Box Tool">
             <BoundingBoxToolbarButton />
+        </SampleDetailsToolbarTooltip>
+        <SampleDetailsToolbarTooltip label="Brush Tool">
+            <BrushToolbarButton />
         </SampleDetailsToolbarTooltip>
     </CardContent>
 </Card>
