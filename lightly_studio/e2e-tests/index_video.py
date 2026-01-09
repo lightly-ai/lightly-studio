10dd534cae6326615023c4cd312b1ea85eb62a4a
Horatiu Almasan <horatiu@lightly.ai>
2026-01-09 13:03:36 +0200

    update after review

--- lightly_studio/e2e-tests/index_video.py
+++ lightly_studio/e2e-tests/index_video.py
@@ -9,20 +9,19 @@ from lightly_studio import db_manager
 from lightly_studio.core.video_dataset import VideoDataset
 from lightly_studio.utils.video_annotations_helpers import load_annotations
 
-if __name__ == "__main__":
-    # Cleanup an existing database
-    db_manager.connect(cleanup_existing=True)
+# Cleanup an existing database
+db_manager.connect(cleanup_existing=True)
 
-    # Create a Dataset from a path
-    dataset = VideoDataset.create()
-    dataset.add_videos_from_path(path="datasets/youtube_vis_50_videos", embed=False)
+# Create a Dataset from a path
+dataset = VideoDataset.create()
+dataset.add_videos_from_path(path="datasets/youtube_vis_50_videos", embed=False)
 
-    # Load annotations
-    load_annotations(
-        session=dataset.session,
-        collection_id=dataset.dataset_id,
-        annotations_path=Path("datasets/youtube_vis_50_videos/train/instances_50.json"),
-    )
+# Load annotations
+load_annotations(
+    session=dataset.session,
+    collection_id=dataset.dataset_id,
+    annotations_path=Path("datasets/youtube_vis_50_videos/train/instances_50.json"),
+)
 
-    # Start the GUI
-    ls.start_gui()
+# Start the GUI
+ls.start_gui()
--- lightly_studio/src/lightly_studio/utils/__init__.py
+++ lightly_studio/src/lightly_studio/utils/__init__.py
@@ -1,11 +1,3 @@
 from .download import download_example_dataset
-from .video_annotations_helpers import (
-    YouTubeVISObjectDetectionInput,
-    load_annotations,
-)
 
-__all__ = [
-    "YouTubeVISObjectDetectionInput",
-    "download_example_dataset",
-    "load_annotations",
-]
+__all__ = ["download_example_dataset"]
