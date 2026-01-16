0b57856a6d7cc26c589cf9069a02fb3c09d8968c
Horatiu Almasan <horatiu@lightly.ai>
2026-01-16 11:03:30 +0200

    remove some redirects

--- lightly_studio_view/src/routes/[...rest]/+page.ts
+++
@@ -1,9 +0,0 @@
-import { redirect } from '@sveltejs/kit';
-import { routeHelpers } from '$lib/routes';
-import type { PageLoad } from './$types';
-
-export const load: PageLoad = async () => {
-    // Catch-all route: redirect any unmatched paths to home
-    // This handles typos like /datas, /dataset, /invalid-path, etc.
-    throw redirect(307, routeHelpers.toHome());
-};
