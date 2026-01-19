28acd457a3a79e98ca6878e6815e84bd9c8de791
michal-lightly <105644579+michal-lightly@users.noreply.github.com>
2026-01-19 12:04:00 +0100

    Prepare Python interface for semantic segmentation table removal (#425)

--- lightly_studio/src/lightly_studio/core/annotation/semantic_segmentation.py
+++ lightly_studio/src/lightly_studio/core/annotation/semantic_segmentation.py
@@ -7,9 +7,7 @@ from sqlmodel import col
 from lightly_studio.core.annotation import Annotation
 from lightly_studio.core.db_field import DBField
 from lightly_studio.models.annotation.annotation_base import AnnotationType
-from lightly_studio.models.annotation.semantic_segmentation import (
-    SemanticSegmentationAnnotationTable,
-)
+from lightly_studio.models.annotation.segmentation import SegmentationAnnotationTable
 
 
 class SemanticSegmentationAnnotation(Annotation):
@@ -18,19 +16,29 @@ class SemanticSegmentationAnnotation(Annotation):
     The properties of the annotation are accessible as attributes of this class.
 
     ```python
-    print(f"Annotation segmentation mask: {annotation.segmentation_mask}"
+    print(f"Annotation x/y coordinates: ({annotation.x},{annotation.y})")
+    print(f"Annotation width and height: {annotation.width}x{annotation.height}")
+    print(f"Annotation segmentation mask: {annotation.segmentation_mask}")
     ```
 
     """
 
-    segmentation_mask = DBField(col(SemanticSegmentationAnnotationTable.segmentation_mask))
-    """Segmentation mask as a list of integers."""
+    x = DBField(col(SegmentationAnnotationTable.x))
+    """X coordinate (px) of the segmentation bounding box."""
+    y = DBField(col(SegmentationAnnotationTable.y))
+    """Y coordinate (px) of the segmentation bounding box."""
+    width = DBField(col(SegmentationAnnotationTable.width))
+    """Width (px) of the segmentation bounding box."""
+    height = DBField(col(SegmentationAnnotationTable.height))
+    """Height (px) of the segmentation bounding box."""
+    segmentation_mask = DBField(col(SegmentationAnnotationTable.segmentation_mask))
+    """Segmentation mask given as a run-length encoding."""
 
-    def __init__(self, inner: SemanticSegmentationAnnotationTable) -> None:
+    def __init__(self, inner: SegmentationAnnotationTable) -> None:
         """Initialize the semantic segmentation annotation.
 
         Args:
-            inner: The SemanticSegmentationAnnotationTable SQLAlchemy model instance.
+            inner: The SegmentationAnnotationTable SQLAlchemy model instance.
         """
         if inner.annotation_base.annotation_type != AnnotationType.SEMANTIC_SEGMENTATION:
             raise ValueError("Expected annotation type: semantic segmentation")
