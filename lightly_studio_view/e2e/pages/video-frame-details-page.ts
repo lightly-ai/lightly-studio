75516c86d7e8e0c69486d8d7656f9202fbdd9c42
Leonardo Rosa <43169851+LeonardoRosaa@users.noreply.github.com>
2026-01-27 10:05:27 -0300

    Add E2E test for video frame captions (#467)

---
+++ lightly_studio_view/e2e/pages/video-frame-details-page.ts
@@ -0,0 +1,53 @@
+import { expect, type Page } from '@playwright/test';
+import { CaptionUtils } from '../caption-utils';
+
+export class VideoFrameDetailsPage {
+    private captionUtils: CaptionUtils;
+
+    constructor(public readonly page: Page) {
+        this.page = page;
+        this.captionUtils = new CaptionUtils(page);
+    }
+
+    async pageIsReady() {
+        await expect(this.getSampleDetails()).toBeVisible();
+    }
+
+    getSampleDetails() {
+        return this.page.getByTestId('video-frame-details');
+    }
+
+    async clickEditButton() {
+        await this.page.getByTestId('header-editing-mode-button').click();
+    }
+
+    // Captions
+
+    getCaptionCount() {
+        return this.captionUtils.getCaptionCount();
+    }
+
+    getNthCaption(index: number) {
+        return this.captionUtils.getNthCaption(index);
+    }
+
+    getNthCaptionText(index: number) {
+        return this.captionUtils.getNthCaptionText(index);
+    }
+
+    getNthCaptionInput(index: number) {
+        return this.captionUtils.getNthCaptionInput(index);
+    }
+
+    async addCaption() {
+        await this.captionUtils.addCaption();
+    }
+
+    async deleteNthCaption(index: number) {
+        await this.captionUtils.deleteNthCaption(index);
+    }
+
+    async updateNthCaption(index: number, text: string) {
+        await this.captionUtils.updateNthCaption(index, text);
+    }
+}
