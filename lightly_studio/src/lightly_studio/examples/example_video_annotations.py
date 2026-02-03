f290ce53655dad6f90b28b9539f58390ba38b633
JonasWurst <jonas@lightly.ai>
2026-02-03 16:23:01 +0100

    simplify example

--- lightly_studio/src/lightly_studio/examples/example_video_annotations.py
+++ lightly_studio/src/lightly_studio/examples/example_video_annotations.py
@@ -8,26 +8,25 @@ import lightly_studio as ls
 from lightly_studio import db_manager
 from lightly_studio.core.video_dataset import VideoDataset
 
-if __name__ == "__main__":
-    # Read environment variables
-    env = Env()
-    env.read_env()
+# Read environment variables
+env = Env()
+env.read_env()
 
-    # Cleanup an existing database
-    db_manager.connect(cleanup_existing=True)
+# Cleanup an existing database
+db_manager.connect(cleanup_existing=True)
 
-    # Define the path to the dataset directory
-    dataset_path = env.path("EXAMPLES_VIDEO_DATASET_PATH", "/path/to/your/dataset")
-    annotations_path = env.path(
-        "EXAMPLES_VIDEO_YVIS_JSON_PATH", "/path/to/your/dataset/instances.json"
-    )
+# Define the path to the dataset directory
+dataset_path = env.path("EXAMPLES_VIDEO_DATASET_PATH", "/path/to/your/dataset")
+annotations_path = env.path(
+    "EXAMPLES_VIDEO_YVIS_JSON_PATH", "/path/to/your/dataset/instances.json"
+)
 
-    # Create a Dataset from a path
-    dataset = VideoDataset.create()
-    dataset.add_videos_from_youtube_vis(
-        annotations_json=annotations_path,
-        path=dataset_path,
-    )
+# Create a Dataset from a path
+dataset = VideoDataset.create()
+dataset.add_videos_from_youtube_vis(
+    annotations_json=annotations_path,
+    path=dataset_path,
+)
 
-    # Start the GUI
-    ls.start_gui()
+# Start the GUI
+ls.start_gui()
