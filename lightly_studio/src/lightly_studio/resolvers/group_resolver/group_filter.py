d1f8deb55cb549cd026ab3d5d6ec2924ec56973a
Kondrat Shmoylov <kondrat.shmoylov@gmail.com>
2026-02-06 15:45:31 +0100

    feat: introduce initial filtering for groups

---
+++ lightly_studio/src/lightly_studio/resolvers/group_resolver/group_filter.py
@@ -0,0 +1,21 @@
+"""Utility functions for building database queries for groups."""
+
+from typing import Optional
+
+from pydantic import BaseModel
+
+from lightly_studio.resolvers.sample_resolver.sample_filter import SampleFilter
+from lightly_studio.type_definitions import QueryType
+
+
+class GroupFilter(BaseModel):
+    """Encapsulates filter parameters for querying groups."""
+
+    sample_filter: Optional[SampleFilter] = None
+
+    def apply(self, query: QueryType) -> QueryType:
+        """Apply the filters to the given query."""
+        if self.sample_filter is not None:
+            query = self.sample_filter.apply(query)
+
+        return query  # noqa: RET504
