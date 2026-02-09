8d7b27004d6640eeaf9b45a86353c7934e9f6fb5
LeonardoRosaa <leonardoorosaa16@gmail.com>
2026-02-09 07:17:52 -0300

    fix: refresh grid side panel

--- lightly_studio_view/src/lib/hooks/useCreateAnnotation/useCreateAnnotation.ts
+++ lightly_studio_view/src/lib/hooks/useCreateAnnotation/useCreateAnnotation.ts
@@ -2,14 +2,22 @@ import {
     type AnnotationCreateInput,
     type CreateAnnotationResponse
 } from '$lib/api/lightly_studio_local';
-import { createAnnotationMutation } from '$lib/api/lightly_studio_local/@tanstack/svelte-query.gen';
-import { createMutation } from '@tanstack/svelte-query';
+import { countAnnotationsByCollectionOptions, createAnnotationMutation } from '$lib/api/lightly_studio_local/@tanstack/svelte-query.gen';
+import { createMutation, useQueryClient } from '@tanstack/svelte-query';
 
 import { get } from 'svelte/store';
 
 export const useCreateAnnotation = ({ collectionId }: { collectionId: string }) => {
     const mutation = createMutation(createAnnotationMutation());
-
+        const client = useQueryClient();
+    
+    const refetch = () => {
+            client.invalidateQueries({
+                queryKey: countAnnotationsByCollectionOptions({
+                    path: { collection_id: collectionId }
+                }).queryKey
+            });
+        };
     // We need to have this subscription to get onSuccess/onError events
     mutation.subscribe(() => undefined);
 
@@ -24,6 +32,7 @@ export const useCreateAnnotation = ({ collectionId }: { collectionId: string }) 
                 },
                 {
                     onSuccess: (data) => {
+                        refetch()
                         resolve(data);
                     },
                     onError: (error) => {
