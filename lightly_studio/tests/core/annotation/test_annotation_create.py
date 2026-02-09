f55bd3f570bb8ea2170307213dbd21cfc88659ff
Lukáš Poláček <lukas@lightly.ai>
2026-02-09 15:39:53 +0100

    Change test order

--- lightly_studio/tests/core/annotation/test_annotation_create.py
+++ lightly_studio/tests/core/annotation/test_annotation_create.py
@@ -95,6 +95,21 @@ def test_create_semantic_segmentation_from_binary_mask() -> None:
     assert result.segmentation_mask == [23, 2, 8, 2, 65]
 
 
+def test_create_semantic_segmentation_from_binary_mask_empty() -> None:
+    mask = np.zeros((10, 10), dtype=np.int_)
+    result = CreateSemanticSegmentation.from_binary_mask(
+        label="empty",
+        binary_mask=mask,
+    )
+
+    # The mask is empty.
+    assert result.x == 0
+    assert result.y == 0
+    assert result.width == 0
+    assert result.height == 0
+    assert result.segmentation_mask == [100]
+
+
 def test_create_semantic_segmentation_from_rle_mask() -> None:
     rle_mask = [23, 2, 8, 2, 65]  # Corresponds to a 2x2 square at (3,2)
     image_sample = MockImageSample(width=10, height=10)
@@ -113,18 +128,3 @@ def test_create_semantic_segmentation_from_rle_mask() -> None:
     assert result.width == 2
     assert result.height == 2
     assert result.segmentation_mask == rle_mask
-
-
-def test_create_semantic_segmentation_from_binary_mask_empty() -> None:
-    mask = np.zeros((10, 10), dtype=np.int_)
-    result = CreateSemanticSegmentation.from_binary_mask(
-        label="empty",
-        binary_mask=mask,
-    )
-
-    # The mask is empty.
-    assert result.x == 0
-    assert result.y == 0
-    assert result.width == 0
-    assert result.height == 0
-    assert result.segmentation_mask == [100]
