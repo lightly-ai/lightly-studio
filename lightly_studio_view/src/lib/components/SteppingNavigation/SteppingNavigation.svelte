0d9dd4fab46bb21e0bb20c8c6dc93b5cb307c32f
LeonardoRosaa <leonardoorosaa16@gmail.com>
2026-02-11 15:44:03 -0300

    fixed tests

--- lightly_studio_view/src/lib/components/SteppingNavigation/SteppingNavigation.svelte
+++ lightly_studio_view/src/lib/components/SteppingNavigation/SteppingNavigation.svelte
@@ -1,17 +1,16 @@
 <script lang="ts">
-    import { useAnnotationLabelContext } from '$lib/contexts/SampleDetailsAnnotation.svelte';
-
     type SteppingNavigationProps = {
         hasPrevious: boolean;
         hasNext: boolean;
         onNext: () => void;
         onPrevious: () => void;
+        isDrawing?: boolean;
     };
-    const { hasPrevious, hasNext, onNext, onPrevious }: SteppingNavigationProps = $props();
-    const { context: annotationLabelContext } = useAnnotationLabelContext();
+    const { hasPrevious, hasNext, onNext, onPrevious, isDrawing }: SteppingNavigationProps =
+        $props();
 
     const handleKeyDownEvent = (event: KeyboardEvent) => {
-        if (annotationLabelContext.isDrawing) return;
+        if (isDrawing) return;
         switch (event.key) {
             case 'ArrowRight':
                 onNext();
@@ -26,11 +25,11 @@
 {#if hasPrevious}
     <button
         class="absolute left-4 top-1/2 z-30 -translate-y-1/2 rounded-full bg-black/30 p-2 text-white opacity-50 transition-opacity hover:opacity-100"
-        class:pointer-events-none={annotationLabelContext.isDrawing}
-        class:opacity-20={annotationLabelContext.isDrawing}
+        class:pointer-events-none={isDrawing}
+        class:opacity-20={isDrawing}
         onclick={onPrevious}
         aria-label="Previous sample"
-        disabled={annotationLabelContext.isDrawing}
+        disabled={isDrawing}
     >
         <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -51,11 +50,11 @@
 {#if hasNext}
     <button
         class="absolute right-4 top-1/2 z-30 -translate-y-1/2 rounded-full bg-black/30 p-2 text-white opacity-50 transition-opacity hover:opacity-100"
-        class:pointer-events-none={annotationLabelContext.isDrawing}
-        class:opacity-20={annotationLabelContext.isDrawing}
+        class:pointer-events-none={isDrawing}
+        class:opacity-20={isDrawing}
         onclick={onNext}
         aria-label="Next sample"
-        disabled={annotationLabelContext.isDrawing}
+        disabled={isDrawing}
     >
         <svg
             xmlns="http://www.w3.org/2000/svg"
