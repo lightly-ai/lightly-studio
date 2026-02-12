1c3871a42c6f193cd6f860d098a56d1be0009648
IgorSusmelj <igor_7779@hotmail.com>
2026-02-12 09:19:17 +0100

    Add unit test to make sure the GUI works when toggling show/hide of the embedding plot

--- lightly_studio_view/e2e/general/bugs/LIG-7691-plot-visible-only-in-samples.e2e-test.ts
+++ lightly_studio_view/e2e/general/bugs/LIG-7691-plot-visible-only-in-samples.e2e-test.ts
@@ -3,16 +3,34 @@ import { expect, test } from '../../utils';
 test('Plot is visible only on samples page', async ({ samplesPage, page }) => {
     await samplesPage.goto();
 
-    expect(page.getByTestId('toggle-plot-button')).toBeVisible();
+    const togglePlotButton = page.getByTestId('toggle-plot-button');
+    const plotPanel = page.getByTestId('plot-panel');
+    const plotControls = page.getByTestId('plot-panel-controls');
+    const resetViewButton = page.getByTestId('plot-reset-view-button');
+    const rectangleSelectionButton = page.locator('button[title*="rectangle selection mode"]');
+    const lassoSelectionButton = page.locator('button[title*="lasso selection mode"]');
 
-    // TODO(Horatiu, 10/2025): Check why pressing the plot button will cause issues using Chromium
-    // await page.getByTestId('toggle-plot-button').click();
-    // expect(page.getByTestId('plot-panel')).toBeVisible();
+    await expect(togglePlotButton).toBeVisible();
 
-    page.getByTestId('navigation-menu-annotations').click();
+    // TODO(Horatiu, 10/2025): Historically, toggling the plot was flaky in Chromium.
+    // Keep this regression coverage and monitor CI stability.
+    // Repeat the open/close cycle to catch intermittent rendering issues.
+    for (let i = 0; i < 3; i++) {
+        await togglePlotButton.click();
+        await expect(plotPanel).toBeVisible();
+        await expect(plotControls).toBeVisible();
+        await expect(resetViewButton).toBeVisible();
+        await expect(rectangleSelectionButton).toBeVisible();
+        await expect(lassoSelectionButton).toBeVisible();
+
+        await togglePlotButton.click();
+        await expect(plotPanel).not.toBeVisible();
+    }
+
+    await page.getByTestId('navigation-menu-annotations').click();
 
     await expect(page.getByTestId('annotations-grid')).toBeVisible({ timeout: 1000 });
 
-    expect(page.getByTestId('toggle-plot-button')).not.toBeVisible();
-    expect(page.getByTestId('plot-panel')).not.toBeVisible();
+    await expect(page.getByTestId('toggle-plot-button')).not.toBeVisible();
+    await expect(page.getByTestId('plot-panel')).not.toBeVisible();
 });
