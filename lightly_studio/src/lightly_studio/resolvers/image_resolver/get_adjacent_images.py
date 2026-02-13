cb7352c49360aacdf4f4f27002a93dbc1ec78bf2
LeonardoRosaa <leonardoorosaa16@gmail.com>
2026-02-13 13:53:42 -0300

    fix static-checks

--- lightly_studio/src/lightly_studio/resolvers/image_resolver/get_adjacent_images.py
+++ lightly_studio/src/lightly_studio/resolvers/image_resolver/get_adjacent_images.py
@@ -2,10 +2,12 @@
 
 from __future__ import annotations
 
+from typing import Any
 from uuid import UUID
 
 from sqlalchemy import func
 from sqlmodel import Session, col, select
+from sqlmodel.sql.expression import Select
 
 from lightly_studio.models.adjacents import AdjancentResultView
 from lightly_studio.models.image import ImageTable
@@ -57,60 +59,21 @@ def _get_all_with_similarity(  # noqa: PLR0913
     sample_ids: list[UUID] | None,
 ) -> AdjancentResultView:
     """Get the adjacent images for a given sample ID, using similarity search."""
-    ordering_expression = col(ImageTable.file_path_abs).asc()
-
-    samples_query = select(
-        col(ImageTable.sample_id).label("sample_id"),
-        func.lag(col(ImageTable.sample_id))
-        .over(order_by=ordering_expression)
-        .label("sample_previous_id"),
-        func.lead(col(ImageTable.sample_id))
-        .over(order_by=ordering_expression)
-        .label("sample_next_id"),
-        func.row_number().over(order_by=ordering_expression).label("row_number"),
-    ).select_from(ImageTable)
+    base_query = _base_query()
 
     samples_query = apply_similarity_join(
-        query=samples_query,
+        query=base_query,
         sample_id_column=col(ImageTable.sample_id),
         embedding_model_id=embedding_model_id,
     )
-    samples_query = samples_query.join(ImageTable.sample).where(
-        SampleTable.collection_id == collection_id
-    )
 
-    if filters:
-        samples_query = filters.apply(samples_query)
-
-    # TODO(Leonardo, 02/2026): Consider adding sample_ids to the filters.
-    if sample_ids:
-        samples_query = samples_query.where(col(ImageTable.sample_id).in_(sample_ids))
-
-    adjacents_subquery = samples_query.subquery("adjacent_samples")
-
-    adjacency_row = session.exec(
-        select(
-            adjacents_subquery.c.sample_previous_id,
-            adjacents_subquery.c.sample_id,
-            adjacents_subquery.c.sample_next_id,
-            adjacents_subquery.c.row_number,
-        ).where(adjacents_subquery.c.sample_id == sample_id)
-    ).first()
-
-    if adjacency_row is None:
-        return AdjancentResultView(
-            sample_previous_id=None,
-            sample_id=sample_id,
-            sample_next_id=None,
-            position=None,
-        )
-
-    position = int(adjacency_row.row_number) - 1 if adjacency_row.row_number is not None else None
-    return AdjancentResultView(
-        sample_previous_id=adjacency_row.sample_previous_id,
-        sample_id=adjacency_row.sample_id,
-        sample_next_id=adjacency_row.sample_next_id,
-        position=position,
+    return _build_query(
+        query=samples_query,
+        session=session,
+        collection_id=collection_id,
+        sample_id=sample_id,
+        filters=filters,
+        sample_ids=sample_ids,
     )
 
 
@@ -121,9 +84,22 @@ def _get_all_without_similarity(
     filters: ImageFilter | None,
     sample_ids: list[UUID] | None,
 ) -> AdjancentResultView:
+    base_query = _base_query()
+
+    return _build_query(
+        query=base_query,
+        session=session,
+        collection_id=collection_id,
+        sample_id=sample_id,
+        filters=filters,
+        sample_ids=sample_ids,
+    )
+
+
+def _base_query() -> Select[Any]:
     ordering_expression = col(ImageTable.file_path_abs).asc()
 
-    samples_query = select(
+    return select(
         col(ImageTable.sample_id).label("sample_id"),
         func.lag(col(ImageTable.sample_id))
         .over(order_by=ordering_expression)
@@ -133,9 +109,17 @@ def _get_all_without_similarity(
         .label("sample_next_id"),
         func.row_number().over(order_by=ordering_expression).label("row_number"),
     ).select_from(ImageTable)
-    samples_query = samples_query.join(ImageTable.sample).where(
-        SampleTable.collection_id == collection_id
-    )
+
+
+def _build_query(  # noqa: PLR0913
+    query: Any,
+    session: Session,
+    collection_id: UUID,
+    sample_id: UUID,
+    filters: ImageFilter | None,
+    sample_ids: list[UUID] | None,
+) -> AdjancentResultView:
+    samples_query = query.join(ImageTable.sample).where(SampleTable.collection_id == collection_id)
 
     if filters:
         samples_query = filters.apply(samples_query)
@@ -163,10 +147,12 @@ def _get_all_without_similarity(
             position=None,
         )
 
-    position = int(adjacency_row.row_number) - 1 if adjacency_row.row_number is not None else None
+    sample_previous_id, sample_id_row, sample_next_id, row_number = adjacency_row
+
+    position = int(row_number) - 1 if row_number is not None else None
     return AdjancentResultView(
-        sample_previous_id=adjacency_row.sample_previous_id,
-        sample_id=adjacency_row.sample_id,
-        sample_next_id=adjacency_row.sample_next_id,
+        sample_previous_id=sample_previous_id,
+        sample_id=sample_id_row,
+        sample_next_id=sample_next_id,
         position=position,
     )
