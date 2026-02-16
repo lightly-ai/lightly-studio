5e842c9a9ae2a423e99696a8e69d6eec3e9d1a93
Kondrat Shmoylov <kondrat.shmoylov@gmail.com>
2026-02-16 16:43:40 +0100

    update comment

--- lightly_studio/tests/resolvers/group_resolver/test_get_group_snapshots.py
+++ lightly_studio/tests/resolvers/group_resolver/test_get_group_snapshots.py
@@ -183,7 +183,7 @@ def test_get_group_snapshots_multiple_groups(db_session: Session) -> None:
     assert group_ids[0] in snapshots
     assert group_ids[1] in snapshots
     assert snapshots[group_ids[0]].sample_id == image1.sample_id
-    # image2 should be returned (created before image3)
+    # Should return image2 (created before image3)
     assert snapshots[group_ids[1]].sample_id == image2.sample_id
 
 
