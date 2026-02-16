e6b3bfe22573c389c70ce00f2172d5209a2d2ea1
Kondrat Shmoylov <kondrat.shmoylov@gmail.com>
2026-02-16 10:20:05 +0100

    rm

--- lightly_studio_view/src/routes/datasets/[dataset_id]/[collection_type]/[collection_id]/groups/+page.svelte
+++ lightly_studio_view/src/routes/datasets/[dataset_id]/[collection_type]/[collection_id]/groups/+page.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-    import Layout from '$lib/components/Layout/Layout.svelte';
-    import LayoutSection from '$lib/components/LayoutSection/LayoutSection.svelte';
     import type { PageData } from './$types';
 
     const { data }: { data: PageData } = $props();
