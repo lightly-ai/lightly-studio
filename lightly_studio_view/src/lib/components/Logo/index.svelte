e9345eb6d36dac1cf3036f87bdd0860cda47e227
Kondrat Shmoylov <kondrat.shmoylov@gmail.com>
2026-02-16 18:07:38 +0100

    use svg

--- lightly_studio_view/src/lib/components/Logo/index.svelte
+++ lightly_studio_view/src/lib/components/Logo/index.svelte
@@ -2,6 +2,6 @@
     import LightlyLogo from './LightlyLogo.svg';
 </script>
 
-<div>
-    <img src={LightlyLogo} alt="LightlyStudio Logo" class="h-8" />
-</div>
+<svg width="200" height="34.4" viewBox="0 0 1846 318" xmlns="http://www.w3.org/2000/svg">
+    <image href={LightlyLogo} width="1846" height="318" image-rendering="optimizeQuality" />
+</svg>
