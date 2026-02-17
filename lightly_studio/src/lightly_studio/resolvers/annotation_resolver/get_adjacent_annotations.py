84b56a4dc935d20d45d1b041b86425add7fcaae3
LeonardoRosaa <leonardoorosaa16@gmail.com>
2026-02-17 12:50:42 -0300

    fix filter by tag ids

--- lightly_studio/src/lightly_studio/resolvers/annotation_resolver/get_adjacent_annotations.py
+++ lightly_studio/src/lightly_studio/resolvers/annotation_resolver/get_adjacent_annotations.py
@@ -5,10 +5,11 @@ from __future__ import annotations
 from typing import Any
 from uuid import UUID
 
+import sqlmodel
 from sqlalchemy import func
 from sqlalchemy.sql.elements import ColumnElement
 from sqlmodel import Session, col, select
-from sqlmodel.sql.expression import Select
+from sqlmodel.sql.expression import Select, SelectOfScalar
 
 from lightly_studio.models.adjacents import AdjacentResultView
 from lightly_studio.models.annotation.annotation_base import AnnotationBaseTable
@@ -27,41 +28,47 @@ def get_adjacent_annotations(
     if not filters.collection_ids:
         raise ValueError("Collection IDs must be provided in filters.")
 
-    base_query = _base_query()
-    base_query = filters.apply(base_query)
-
     return adjacents.get_sample_adjacent_info(
         session=session,
         sample_id=sample_id,
-        samples_query=base_query,
+        samples_query=_build_window_query(filters),
     )
 
 
-def _base_query() -> Select[Any]:
+def _build_window_query(filters: AnnotationsFilter) -> Select[Any]:
+    base_rows: SelectOfScalar[AnnotationBaseTable] = select(AnnotationBaseTable)
+    filtered_rows = filters.apply(base_rows).subquery()
+
+    file_path_abs = func.coalesce(
+        col(ImageTable.file_path_abs),
+        col(VideoTable.file_path_abs),
+        "",
+    )
     ordering_expression: tuple[ColumnElement[Any], ColumnElement[Any], ColumnElement[Any]] = (
-        func.coalesce(ImageTable.file_path_abs, VideoTable.file_path_abs, "").asc(),
-        col(AnnotationBaseTable.created_at).asc(),
-        col(AnnotationBaseTable.sample_id).asc(),
+        file_path_abs.asc(),
+        filtered_rows.c.created_at.asc(),
+        filtered_rows.c.sample_id.asc(),
     )
 
     return (
         select(
-            col(AnnotationBaseTable.sample_id).label("sample_id"),
-            func.lag(col(AnnotationBaseTable.sample_id))
+            filtered_rows.c.sample_id.label("sample_id"),
+            func.lag(filtered_rows.c.sample_id)
             .over(order_by=ordering_expression)
             .label("previous_sample_id"),
-            func.lead(col(AnnotationBaseTable.sample_id))
+            func.lead(filtered_rows.c.sample_id)
             .over(order_by=ordering_expression)
             .label("next_sample_id"),
             func.row_number().over(order_by=ordering_expression).label("row_number"),
         )
-        .select_from(AnnotationBaseTable)
-        .outerjoin(
-            ImageTable, col(ImageTable.sample_id) == col(AnnotationBaseTable.parent_sample_id)
-        )
+        .select_from(filtered_rows)
+        .outerjoin(ImageTable, col(ImageTable.sample_id) == filtered_rows.c.parent_sample_id)
         .outerjoin(
             VideoFrameTable,
-            col(VideoFrameTable.sample_id) == col(AnnotationBaseTable.parent_sample_id),
+            col(VideoFrameTable.sample_id) == filtered_rows.c.parent_sample_id,
         )
-        .outerjoin(VideoTable, col(VideoTable.sample_id) == col(VideoFrameTable.parent_sample_id))
+        .outerjoin(
+            VideoTable,
+            col(VideoTable.sample_id) == sqlmodel.col(VideoFrameTable.parent_sample_id),
+        )
     )
--- lightly_studio/tests/resolvers/annotations/test_get_adjacent_annotations.py
+++ lightly_studio/tests/resolvers/annotations/test_get_adjacent_annotations.py
@@ -2,7 +2,7 @@ import pytest
 from sqlmodel import Session
 
 from lightly_studio.models.collection import SampleType
-from lightly_studio.resolvers import annotation_resolver
+from lightly_studio.resolvers import annotation_resolver, tag_resolver
 from lightly_studio.resolvers.annotations.annotations_filter import AnnotationsFilter
 from tests import helpers_resolvers
 
@@ -229,6 +229,184 @@ def test_get_adjacent_annotations__respects_annotation_filter(test_db: Session) 
     assert result.next_sample_id is None
     assert result.current_sample_position == 2
     assert result.total_count == 2
+
+
+def test_get_adjacent_annotations__respects_annotation_tags(test_db: Session) -> None:
+    collection = helpers_resolvers.create_collection(session=test_db, sample_type=SampleType.IMAGE)
+    collection_id = collection.collection_id
+
+    dog_label = helpers_resolvers.create_annotation_label(
+        session=test_db,
+        dataset_id=collection_id,
+        label_name="dog",
+    )
+
+    image_a = helpers_resolvers.create_image(
+        session=test_db,
+        collection_id=collection_id,
+        file_path_abs="/images/a.png",
+    )
+    image_b = helpers_resolvers.create_image(
+        session=test_db,
+        collection_id=collection_id,
+        file_path_abs="/images/b.png",
+    )
+    image_c = helpers_resolvers.create_image(
+        session=test_db,
+        collection_id=collection_id,
+        file_path_abs="/images/c.png",
+    )
+
+    annotation_a = helpers_resolvers.create_annotation(
+        session=test_db,
+        collection_id=collection_id,
+        sample_id=image_a.sample_id,
+        annotation_label_id=dog_label.annotation_label_id,
+    )
+    annotation_collection_id = annotation_a.sample.collection_id
+    annotation_b = helpers_resolvers.create_annotation(
+        session=test_db,
+        collection_id=collection_id,
+        sample_id=image_b.sample_id,
+        annotation_label_id=dog_label.annotation_label_id,
+    )
+    annotation_c = helpers_resolvers.create_annotation(
+        session=test_db,
+        collection_id=collection_id,
+        sample_id=image_c.sample_id,
+        annotation_label_id=dog_label.annotation_label_id,
+    )
+
+    tag_one = helpers_resolvers.create_tag(
+        session=test_db,
+        collection_id=annotation_collection_id,
+        tag_name="anno-tag-1",
+    )
+    tag_two = helpers_resolvers.create_tag(
+        session=test_db,
+        collection_id=annotation_collection_id,
+        tag_name="anno-tag-2",
+    )
+
+    tag_resolver.add_sample_ids_to_tag_id(
+        session=test_db, tag_id=tag_one.tag_id, sample_ids=[annotation_a.sample_id]
+    )
+    tag_resolver.add_sample_ids_to_tag_id(
+        session=test_db,
+        tag_id=tag_one.tag_id,
+        sample_ids=[annotation_b.sample_id],
+    )
+    tag_resolver.add_sample_ids_to_tag_id(
+        session=test_db, tag_id=tag_two.tag_id, sample_ids=[annotation_b.sample_id]
+    )
+    tag_resolver.add_sample_ids_to_tag_id(
+        session=test_db, tag_id=tag_two.tag_id, sample_ids=[annotation_c.sample_id]
+    )
+
+    result = annotation_resolver.get_adjacent_annotations(
+        session=test_db,
+        sample_id=annotation_b.sample_id,
+        filters=AnnotationsFilter(
+            collection_ids=[annotation_collection_id],
+            annotation_tag_ids=[tag_one.tag_id, tag_two.tag_id],
+        ),
+    )
+
+    assert result is not None
+    assert result.previous_sample_id == annotation_a.sample_id
+    assert result.sample_id == annotation_b.sample_id
+    assert result.next_sample_id == annotation_c.sample_id
+    assert result.current_sample_position == 2
+    assert result.total_count == 3
+
+
+def test_get_adjacent_annotations__respects_sample_tags(test_db: Session) -> None:
+    collection = helpers_resolvers.create_collection(session=test_db, sample_type=SampleType.IMAGE)
+    collection_id = collection.collection_id
+
+    dog_label = helpers_resolvers.create_annotation_label(
+        session=test_db,
+        dataset_id=collection_id,
+        label_name="dog",
+    )
+
+    image_a = helpers_resolvers.create_image(
+        session=test_db,
+        collection_id=collection_id,
+        file_path_abs="/images/a.png",
+    )
+    image_b = helpers_resolvers.create_image(
+        session=test_db,
+        collection_id=collection_id,
+        file_path_abs="/images/b.png",
+    )
+    image_c = helpers_resolvers.create_image(
+        session=test_db,
+        collection_id=collection_id,
+        file_path_abs="/images/c.png",
+    )
+
+    annotation_a = helpers_resolvers.create_annotation(
+        session=test_db,
+        collection_id=collection_id,
+        sample_id=image_a.sample_id,
+        annotation_label_id=dog_label.annotation_label_id,
+    )
+    annotation_collection_id = annotation_a.sample.collection_id
+    annotation_b = helpers_resolvers.create_annotation(
+        session=test_db,
+        collection_id=collection_id,
+        sample_id=image_b.sample_id,
+        annotation_label_id=dog_label.annotation_label_id,
+    )
+    annotation_c = helpers_resolvers.create_annotation(
+        session=test_db,
+        collection_id=collection_id,
+        sample_id=image_c.sample_id,
+        annotation_label_id=dog_label.annotation_label_id,
+    )
+
+    sample_tag_one = helpers_resolvers.create_tag(
+        session=test_db,
+        collection_id=collection_id,
+        tag_name="sample-tag-1",
+    )
+    sample_tag_two = helpers_resolvers.create_tag(
+        session=test_db,
+        collection_id=collection_id,
+        tag_name="sample-tag-2",
+    )
+
+    tag_resolver.add_sample_ids_to_tag_id(
+        session=test_db, tag_id=sample_tag_one.tag_id, sample_ids=[image_a.sample_id]
+    )
+    tag_resolver.add_sample_ids_to_tag_id(
+        session=test_db,
+        tag_id=sample_tag_one.tag_id,
+        sample_ids=[image_b.sample_id],
+    )
+    tag_resolver.add_sample_ids_to_tag_id(
+        session=test_db, tag_id=sample_tag_two.tag_id, sample_ids=[image_b.sample_id]
+    )
+    tag_resolver.add_sample_ids_to_tag_id(
+        session=test_db, tag_id=sample_tag_two.tag_id, sample_ids=[image_c.sample_id]
+    )
+
+    result = annotation_resolver.get_adjacent_annotations(
+        session=test_db,
+        sample_id=annotation_b.sample_id,
+        filters=AnnotationsFilter(
+            collection_ids=[annotation_collection_id],
+            sample_tag_ids=[sample_tag_one.tag_id, sample_tag_two.tag_id],
+        ),
+    )
+
+    assert result is not None
+    assert result.previous_sample_id == annotation_a.sample_id
+    assert result.sample_id == annotation_b.sample_id
+    assert result.next_sample_id == annotation_c.sample_id
+    assert result.current_sample_position == 2
+    assert result.total_count == 3
 
 
 def test_get_adjacent_annotations__returns_none_when_sample_not_in_filter(test_db: Session) -> None:
