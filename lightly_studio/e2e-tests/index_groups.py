b16eb2d0e53f6e5bc9d47337ff6ed62362c53754
Kondrat Shmoylov <kondrat.shmoylov@gmail.com>
2026-02-19 11:55:48 +0100

    update examples

--- lightly_studio/e2e-tests/index_groups.py
+++ lightly_studio/e2e-tests/index_groups.py
@@ -4,9 +4,6 @@ from pathlib import Path
 
 import lightly_studio as ls
 from lightly_studio.core.group.group_dataset import GroupDataset
-from lightly_studio.core.image.create_image import CreateImage
-from lightly_studio.core.video.create_video import CreateVideo
-from lightly_studio.resolvers import collection_resolver, group_resolver
 
 # EDIT THIS
 DATASET_DIR = Path("datasets/midv_2020_10_samples")
@@ -22,14 +19,13 @@ dataset = GroupDataset.create(
         ("clips_video", ls.SampleType.VIDEO),
     ]
 )
-session = dataset.session
 
-# Add samples to the dataset
-# TODO: Switch away from resolvers
-component_collections = collection_resolver.get_group_components(
-    session=session,
-    parent_collection_id=dataset.dataset_id,
-)
+# Create components
+example_group = {
+    "photo": "alb_id.jpg",
+    "scan_upright": "alb_id.jpg",
+    "clips_video": "alb_id.mp4",
+}
 comp_filenames = [
     ("alb_id.jpg", "alb_id.jpg", "alb_id.mp4"),
     ("aze_passport.jpg", "aze_passport.jpg", "aze_passport.mp4"),
@@ -42,42 +38,20 @@ comp_filenames = [
     ("srb_passport.jpg", "srb_passport.jpg", "srb_passport.mp4"),
     ("svk_id.jpg", "svk_id.jpg", "svk_id.mp4"),
 ]
+
 for comp_filename in comp_filenames:
     photo_path = str(DATASET_DIR / "photo" / comp_filename[0])
     scan_path = str(DATASET_DIR / "scan_upright" / comp_filename[1])
     vid_path = str(DATASET_DIR / "clips_video" / comp_filename[2])
 
-    photo_id = CreateImage(path=photo_path).create_in_collection(
-        session=session,
-        collection_id=component_collections["photo"].collection_id,
-    )
-    scan_id = CreateImage(path=scan_path).create_in_collection(
-        session=session,
-        collection_id=component_collections["scan_upright"].collection_id,
-    )
-    vid_id = CreateVideo(path=vid_path).create_in_collection(
-        session=session,
-        collection_id=component_collections["clips_video"].collection_id,
+    dataset.add_group_sample(
+        components={
+            "photo": ls.CreateImage(path=str(photo_path)),
+            "scan_upright": ls.CreateImage(path=str(scan_path)),
+            "clips_video": ls.CreateVideo(path=str(vid_path)),
+        }
     )
 
-    group_resolver.create_many(
-        session=session,
-        collection_id=dataset.dataset_id,
-        groups=[(photo_id, scan_id, vid_id)],
-    )
-
-
-# Optional: Print out the samples
-for sample in dataset:
-    print(f"Group sample ID: {sample.sample_id}")
-    photo_comp = sample["photo"]
-    scan_comp = sample["scan_upright"]
-    vid_comp = sample["clips_video"]
-    if photo_comp and scan_comp and vid_comp:
-        print(
-            f"Group components: {photo_comp.file_name}, {scan_comp.file_name}, "  # type: ignore[attr-defined]
-            f"{vid_comp.file_name}"
-        )
 
 # Enable when frontend is ready
 ls.start_gui()
--- lightly_studio/e2e-tests/index_videos_groups.py
+++ lightly_studio/e2e-tests/index_videos_groups.py
@@ -4,10 +4,7 @@ from pathlib import Path
 
 import lightly_studio as ls
 from lightly_studio.core.group.group_dataset import GroupDataset
-from lightly_studio.core.video.create_video import CreateVideo
-from lightly_studio.resolvers import collection_resolver, group_resolver
 
-# EDIT THIS
 DATASET_DIR = Path("datasets/midv_2020_10_samples")
 
 # Cleanup an existing database
@@ -19,14 +16,11 @@ dataset = GroupDataset.create(
         ("clips_video", ls.SampleType.VIDEO),
     ]
 )
-session = dataset.session
 
-# Add samples to the dataset
-# TODO: Switch away from resolvers
-component_collections = collection_resolver.get_group_components(
-    session=session,
-    parent_collection_id=dataset.dataset_id,
-)
+# Add samples to the dataset using example_group
+example_group = {
+    "clips_video": "alb_id.mp4",
+}
 comp_filenames = [
     "alb_id.mp4",
     "aze_passport.mp4",
@@ -39,29 +33,14 @@ comp_filenames = [
     "srb_passport.mp4",
     "svk_id.mp4",
 ]
-groups = []
 for comp_filename in comp_filenames:
     vid_path = str(DATASET_DIR / "clips_video" / comp_filename)
 
-    vid_id = CreateVideo(path=vid_path).create_in_collection(
-        session=session,
-        collection_id=component_collections["clips_video"].collection_id,
+    dataset.add_group_sample(
+        components={
+            "clips_video": ls.CreateVideo(path=str(vid_path)),
+        }
     )
-    groups.append((vid_id,))
-
-group_resolver.create_many(
-    session=session,
-    collection_id=dataset.dataset_id,
-    groups=groups,
-)
-
-
-# Optional: Print out the samples
-for sample in dataset:
-    print(f"Group sample ID: {sample.sample_id}")
-    vid_comp = sample["clips_video"]
-    if vid_comp:
-        print(f"Group component: {vid_comp.file_name}")  # type: ignore[attr-defined]
 
 # Enable when frontend is ready
 ls.start_gui()
