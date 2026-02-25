eb2e9cc825e5e6113e3a81737f42ed198dbda42f
Horatiu Almasan <horatiu@lightly.ai>
2026-02-25 16:50:42 +0200

    update

--- lightly_studio/src/lightly_studio/resolvers/object_track_resolver/__init__.py
+++ lightly_studio/src/lightly_studio/resolvers/object_track_resolver/__init__.py
@@ -6,12 +6,8 @@ from lightly_studio.resolvers.object_track_resolver.add_annotation_to_object_tra
 from lightly_studio.resolvers.object_track_resolver.create_object_track import (
     create_object_track,
 )
-from lightly_studio.resolvers.object_track_resolver.delete_object_track import delete_object_track
-from lightly_studio.resolvers.object_track_resolver.get_by_id import get_by_id
 
 __all__ = [
     "add_annotation_to_object_track",
     "create_object_track",
-    "delete_object_track",
-    "get_by_id",
 ]
