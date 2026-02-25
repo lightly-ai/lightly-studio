7ae45d91f2a4ab51c7c516d4e17dc6e4480293c5
JonasWurst <jonas@lightly.ai>
2026-02-25 16:18:07 +0100

    fix format

--- lightly_studio/tests/plugins/test_operator_context.py
+++ lightly_studio/tests/plugins/test_operator_context.py
@@ -1,7 +1,7 @@
 import pytest
 
 from lightly_studio.models.collection import SampleType
-from lightly_studio.plugins import operator_context 
+from lightly_studio.plugins import operator_context
 from lightly_studio.plugins.operator_context import OperatorScope
 
 
@@ -9,10 +9,12 @@ from lightly_studio.plugins.operator_context import OperatorScope
 def test_get_allowed_scopes_for_collection(sample_type: SampleType) -> None:
     # Make sure each known sampletype can be mapped to a operator type
     scope = OperatorScope(sample_type.value)
-    assert operator_context.get_allowed_scopes_for_collection(sample_type=sample_type, is_root_collection=False) == [
-        scope
-    ]
-    assert operator_context.get_allowed_scopes_for_collection(sample_type=sample_type, is_root_collection=True) == [
+    assert operator_context.get_allowed_scopes_for_collection(
+        sample_type=sample_type, is_root_collection=False
+    ) == [scope]
+    assert operator_context.get_allowed_scopes_for_collection(
+        sample_type=sample_type, is_root_collection=True
+    ) == [
         OperatorScope.ROOT,
         scope,
     ]
