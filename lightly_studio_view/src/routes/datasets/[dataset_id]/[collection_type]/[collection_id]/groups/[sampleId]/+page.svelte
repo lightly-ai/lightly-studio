237bcb925f787d157aeda4abb7e3938fcf0528f4
Kondrat Shmoylov <kondrat.shmoylov@gmail.com>
2026-02-26 17:37:34 +0100

    disable layout for now

--- lightly_studio_view/src/routes/datasets/[dataset_id]/[collection_type]/[collection_id]/groups/[sampleId]/+page.svelte
+++ lightly_studio_view/src/routes/datasets/[dataset_id]/[collection_type]/[collection_id]/groups/[sampleId]/+page.svelte
@@ -5,5 +5,5 @@
 </script>
 
 <div class="flex h-full w-full space-x-4 px-4 pb-4" data-testid="sample-details">
-    <div class="h-full w-full space-y-6 rounded-[1vw] bg-card p-4">Group details</div>
+    <div class="h-full w-full space-y-6 rounded-[1vw] bg-card p-4">Group details3</div>
 </div>
