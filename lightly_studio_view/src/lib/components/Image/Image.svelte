e1195f9c3fd3ebf554befde6fe18dceef2bfead5
Kondrat Shmoylov <kondrat.shmoylov@gmail.com>
2026-02-27 14:17:25 +0100

    feat: introduce base component to render images

---
+++ lightly_studio_view/src/lib/components/Image/Image.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import { cn } from '$lib/utils';
+    import type { HTMLAttributes } from 'svelte/elements';
+
+    const {
+        className,
+        src,
+        imgProps
+    }: {
+        className?: string;
+        src?: string;
+        imgProps?: HTMLAttributes<HTMLImageElement>;
+    } = $props();
+</script>
+
+<img
+    {src}
+    alt={src}
+    class={cn('image rounded-lg bg-black', className)}
+    loading="lazy"
+    {...imgProps}
+/>
