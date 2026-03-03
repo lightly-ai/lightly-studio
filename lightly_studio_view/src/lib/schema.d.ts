33f7531308b38027867d6d20afdc45d02d081037
JonasWurst <jonas@lightly.ai>
2026-03-03 15:04:04 +0100

    update schema

--- lightly_studio_view/src/lib/schema.d.ts
+++ lightly_studio_view/src/lib/schema.d.ts
@@ -1352,8 +1352,7 @@ export interface paths {
          *
          *     Args:
          *         operator_id: The ID of the operator to execute.
-         *         collection_id: The ID of the collection to operate on.
-         *         request: The execution request containing parameters and optional context.
+         *         request: The execution request containing parameters and context.
          *         session: Database session.
          *
          *     Returns:
@@ -5746,9 +5745,7 @@ export interface operations {
     };
     execute_operator: {
         parameters: {
-            query: {
-                collection_id: string;
-            };
+            query?: never;
             header?: never;
             path: {
                 operator_id: string;
