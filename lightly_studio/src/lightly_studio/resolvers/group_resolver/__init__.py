c39fbe4ada92db1967d12a859778d0efe06e7421
Kondrat Shmoylov <kondrat.shmoylov@gmail.com>
2026-03-04 14:18:37 +0100

    rm get_group_component_details_by_ids

--- lightly_studio/src/lightly_studio/resolvers/group_resolver/__init__.py
+++ lightly_studio/src/lightly_studio/resolvers/group_resolver/__init__.py
@@ -6,10 +6,7 @@ from lightly_studio.resolvers.group_resolver.get_by_id import get_by_id
 from lightly_studio.resolvers.group_resolver.get_collection_id_by_group_id import (
     get_collection_id_by_group_id,
 )
-from lightly_studio.resolvers.group_resolver.get_group_component_details_by_ids import (
-    get_group_component_details_by_ids,
-)
-from lightly_studio.resolvers.group_resolver.get_group_component_with_type import (
+    from lightly_studio.resolvers.group_resolver.get_group_component_with_type import (
     get_group_component_with_type,
 )
 from lightly_studio.resolvers.group_resolver.get_group_components_as_dict import (
@@ -28,7 +25,6 @@ __all__ = [
     "get_all",
     "get_by_id",
     "get_collection_id_by_group_id",
-    "get_group_component_details_by_ids",
     "get_group_component_with_type",
     "get_group_components_as_dict",
     "get_group_components_by_group_id",
