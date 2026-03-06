c2b6fa9c8e9aa6e2d380ff34c4d61f476bbea118
Horatiu Almasan <horatiu@lightly.ai>
2026-03-06 18:59:26 +0200

    Add todo

--- lightly_studio_view/e2e/pages/video-frames-page.ts
+++ lightly_studio_view/e2e/pages/video-frames-page.ts
@@ -10,6 +10,7 @@ export class VideoFramesPage {
         await this.page.getByTestId('navigation-menu-frames').click();
 
         // Wait for frame grid items to be visible
+        // TODO(Horatiu 01/2026): 20s timeout might be too long, investigate if we can reduce it.
         await expect(this.getVideoFrames().first()).toBeVisible({
             timeout: 20000
         });
