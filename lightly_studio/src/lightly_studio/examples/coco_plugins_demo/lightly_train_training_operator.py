bf08bc8d95eb8e4a21521f6cd7b929571a434a49
JonasWurst <jonas@lightly.ai>
2026-03-11 20:34:56 +0100

    Operator Context: update LightlyTrain Examples

--- lightly_studio/src/lightly_studio/examples/coco_plugins_demo/lightly_train_training_operator.py
+++ lightly_studio/src/lightly_studio/examples/coco_plugins_demo/lightly_train_training_operator.py
@@ -15,15 +15,13 @@ from lightly_studio.examples.coco_plugins_demo import lightly_train_inference_op
 from lightly_studio.plugins.base_operator import BaseOperator, OperatorResult
 from lightly_studio.plugins.operator_context import ExecutionContext, OperatorScope
 from lightly_studio.plugins.parameter import BaseParameter, IntParameter, StringParameter
-from lightly_studio.resolvers import image_resolver, tag_resolver
+from lightly_studio.resolvers import image_resolver
 from lightly_studio.resolvers.image_filter import ImageFilter
 from lightly_studio.resolvers.sample_resolver.sample_filter import SampleFilter
 
-DEFAULT_INPUT_TAG = "labeled"
 DEFAULT_MODEL_NAME = lightly_train_inference_operator.DEFAULT_MODEL_NAME
 DEFAULT_EPOCHS = 50
 
-PARAM_INPUT_TAG = "input_tag"
 PARAM_MODEL_NAME = "model_name"
 PARAM_EPOCHS = "epochs"
 
@@ -44,12 +42,6 @@ class LightlyTrainObjectDetectionTrainingOperator(BaseOperator):
                 required=True,
                 default=DEFAULT_MODEL_NAME,
                 description="LightlyTrain model name to train.",
-            ),
-            StringParameter(
-                name=PARAM_INPUT_TAG,
-                required=True,
-                default=DEFAULT_INPUT_TAG,
-                description="Tag of samples to train on.",
             ),
             IntParameter(
                 name=PARAM_EPOCHS,
@@ -75,31 +67,21 @@ class LightlyTrainObjectDetectionTrainingOperator(BaseOperator):
         collection_id = context.collection_id
         model_name = str(parameters.get(PARAM_MODEL_NAME, DEFAULT_MODEL_NAME))
         checkpoint_name = model_name
-        input_tag = str(parameters.get(PARAM_INPUT_TAG, DEFAULT_INPUT_TAG))
         epochs = int(parameters.get(PARAM_EPOCHS, DEFAULT_EPOCHS))
 
         time.sleep(3)
 
-        input_tag_entry = tag_resolver.get_by_name(
-            session=session,
-            tag_name=input_tag,
-            collection_id=collection_id,
-        )
-        if input_tag_entry is None:
-            return OperatorResult(
-                success=True,
-                message=(
-                    f"No samples found for tag '{input_tag}'. "
-                    f"Using cached checkpoint '{checkpoint_name}'."
-                ),
-            )
+        context_filter = None
+        if context.context_filter:
+            if isinstance(context.context_filter, SampleFilter):
+                context_filter = ImageFilter(sample_filter=context.context_filter)
+            elif isinstance(context.context_filter, ImageFilter):
+                context_filter = context.context_filter
 
         samples_result = image_resolver.get_all_by_collection_id(
             session=session,
             collection_id=collection_id,
-            filters=ImageFilter(
-                sample_filter=SampleFilter(tag_ids=[input_tag_entry.tag_id]),
-            ),
+            filters=context_filter
         )
         sample_count = samples_result.total_count
 
