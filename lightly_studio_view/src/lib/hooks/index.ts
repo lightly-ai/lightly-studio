f7baecca12c0472d7aa8a1818c36b2c49349c099
Kondrat Shmoylov <kondrat.shmoylov@gmail.com>
2026-03-17 17:11:25 +0100

    update

--- lightly_studio_view/src/lib/hooks/index.ts
+++ lightly_studio_view/src/lib/hooks/index.ts
@@ -1,5 +1,1 @@
-export { useCaptions } from '$lib/hooks/useCaptions/useCaptions';
-export { useRemoveTagFromSample } from '$lib/hooks/useRemoveTagFromSample/useRemoveTagFromSample';
-export { useCollectionWithChildren } from '$lib/hooks/useCollection/useCollection';
-export { useVideo } from '$lib/hooks/useVideo/useVideo';
 export { useVideoFrames } from '$lib/hooks/useVideoFrames/useVideoFrames';
