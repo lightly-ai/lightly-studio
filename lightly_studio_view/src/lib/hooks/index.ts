2b558109e6448e225a137dfa23460ae2843cc6bd
Kondrat Shmoylov <kondrat.shmoylov@gmail.com>
2026-03-17 16:52:54 +0100

    introduce hok to get frame information

---
+++ lightly_studio_view/src/lib/hooks/index.ts
@@ -0,0 +1,5 @@
+export { useCaptions } from '$lib/hooks/useCaptions/useCaptions';
+export { useRemoveTagFromSample } from '$lib/hooks/useRemoveTagFromSample/useRemoveTagFromSample';
+export { useCollectionWithChildren } from '$lib/hooks/useCollection/useCollection';
+export { useVideo } from '$lib/hooks/useVideo/useVideo';
+export { useVideoFrames } from '$lib/hooks/useVideoFrames/useVideoFrames';
