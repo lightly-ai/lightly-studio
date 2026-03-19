7911afff92209ddf7d81641077edf44b3bdd028a
Kondrat Shmoylov <kondrat.shmoylov@gmail.com>
2026-03-19 14:28:01 +0100

    rm

--- lightly_studio_view/src/lib/hooks/useVideo/useVideo.ts
+++ lightly_studio_view/src/lib/hooks/useVideo/useVideo.ts
@@ -1,7 +1,7 @@
 import { getVideoByIdOptions } from '$lib/api/lightly_studio_local/@tanstack/svelte-query.gen';
 import { createQuery } from '@tanstack/svelte-query';
 
-export const useVideo = ({ sampleId }: { sampleId: string; collectionId: string }) => {
+export const useVideo = ({ sampleId }: { sampleId: string }) => {
     const readVideo = getVideoByIdOptions({
         path: {
             sample_id: sampleId
