33e437407a065a0dc60e450d8724faf8b2786671
IgorSusmelj <igor_7779@hotmail.com>
2026-03-26 21:41:32 +0100

    Reduce number of items in grid view page from 50 to 32

--- lightly_studio/src/lightly_studio/examples/example.py
+++ lightly_studio/src/lightly_studio/examples/example.py
@@ -12,14 +12,16 @@ env.read_env()
 # Cleanup an existing database
 db_manager.connect(cleanup_existing=True)
 
-# Define the path to the dataset directory
-dataset_path = env.path("EXAMPLES_DATASET_PATH", "/path/to/your/dataset")
+# Define data paths
+dataset_path = env.path("EXAMPLES_VIDEO_DATASET_PATH", "/path/to/your/dataset")
+annotations_path = env.path("EXAMPLES_VIDEO_YVIS_JSON_PATH", "/path/to/your/dataset/instances.json")
 
 # Create a Dataset from a path
-dataset = ls.ImageDataset.create()
-dataset.add_images_from_path(path=dataset_path)
-
-for sample in dataset:
-    print(sample)
+dataset = ls.VideoDataset.create()
+dataset.add_videos_from_youtube_vis(
+    annotations_json=annotations_path,
+    videos_path=dataset_path,
+    annotation_type=ls.AnnotationType.INSTANCE_SEGMENTATION,
+)
 
 ls.start_gui()
