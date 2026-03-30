a0c41111e2ae5d340596c713fd32543e7ce13e0f
Lukáš Poláček <lukas@lightly.ai>
2026-03-30 10:16:39 +0200

    Add `dataset_resolver.get_root_collection()`
    Even though this function is only used once now, we should have a safe
    method for future users who want this information.

--- lightly_studio/src/lightly_studio/resolvers/dataset_resolver/__init__.py
+++ lightly_studio/src/lightly_studio/resolvers/dataset_resolver/__init__.py
@@ -1,5 +1,6 @@
 """Resolvers for dataset operations."""
 
 from lightly_studio.resolvers.dataset_resolver.get_hierarchy import get_hierarchy
+from lightly_studio.resolvers.dataset_resolver.get_root_collection import get_root_collection
 
-__all__ = ["get_hierarchy"]
+__all__ = ["get_hierarchy", "get_root_collection"]
