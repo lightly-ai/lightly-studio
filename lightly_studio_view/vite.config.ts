29f75a6fc10c7a95fe36421206c39d079be23bb4
Kondrat Shmoylov <kondrat.shmoylov@gmail.com>
2026-04-01 10:18:01 +0200

    feat: introduce  chunk size report (#863)

--- lightly_studio_view/vite.config.ts
+++ lightly_studio_view/vite.config.ts
@@ -4,6 +4,15 @@ import { sveltekit } from '@sveltejs/kit/vite';
 export default defineConfig({
     plugins: [sveltekit()],
 
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks: undefined // Let SvelteKit handle chunking
+            }
+        },
+        chunkSizeWarningLimit: 500
+    },
+
     test: {
         include: ['src/**/*.{test,spec}.{js,ts}']
     }
