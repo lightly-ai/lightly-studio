b6741d117ec2119cfaf16d2c2dd57aa497198969
Mihnea Necsulescu <mihneanecsulescu@gmail.com>
2026-04-03 14:18:04 +0300

    bug fix - clear specific cache only

--- lightly_studio/src/lightly_studio/api/routes/api/enterprise.py
+++ lightly_studio/src/lightly_studio/api/routes/api/enterprise.py
@@ -5,7 +5,6 @@ from __future__ import annotations
 import os
 
 from fastapi import APIRouter
-from fsspec.spec import AbstractFileSystem
 
 enterprise_router = APIRouter()
 
@@ -14,9 +13,13 @@ enterprise_router = APIRouter()
 def refresh_cloud_credentials(credentials: dict[str, str]) -> None:
     """Receive cloud storage credentials.
 
-    Sets the credentials as environment variables and clears all fsspec
-    filesystem instance caches so that subsequent file operations pick up the
-    new credentials.
+    Sets the credentials as environment variables and clears the S3 fsspec
+    instance cache so that subsequent file operations pick up the new
+    credentials.
     """
     os.environ.update(credentials)
-    AbstractFileSystem.clear_instance_cache()
+
+    # We currently support only AWS - this will need to be updated once we add support for other providers.
+    from s3fs import S3FileSystem  # lazy: s3fs is an optional dependency
+
+    S3FileSystem.clear_instance_cache()
