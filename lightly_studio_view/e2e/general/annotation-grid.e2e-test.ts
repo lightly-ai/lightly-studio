bf2d510d4d4d41a548fbf36ddbaa04113e3d0de7
Michal Szabados <michal@lightly.ai>
2026-04-10 17:06:42 +0200

    Undo undo

--- lightly_studio_view/e2e/general/annotation-grid.e2e-test.ts
+++ lightly_studio_view/e2e/general/annotation-grid.e2e-test.ts
@@ -94,9 +94,6 @@ test('We can see clicked element when navigating back from details', async ({
     await annotationsPage.getAnnotationByIndex(20).scrollIntoViewIfNeeded();
 
     expect(
-        await isInViewport({ element: annotationsPage.getAnnotationByIndex(0), viewport })
-    ).toBeFalsy();
-    expect(
         await isInViewport({ element: annotationsPage.getAnnotationByIndex(20), viewport })
     ).toBeTruthy();
 
@@ -108,9 +105,6 @@ test('We can see clicked element when navigating back from details', async ({
 
     await expect(viewport).toBeVisible();
 
-    expect(
-        await isInViewport({ element: annotationsPage.getAnnotationByIndex(0), viewport })
-    ).toBeFalsy();
     expect(
         await isInViewport({ element: annotationsPage.getAnnotationByIndex(20), viewport })
     ).toBeTruthy();
