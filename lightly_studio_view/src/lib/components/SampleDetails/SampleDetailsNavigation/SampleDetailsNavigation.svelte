a02464147bf8e6a6ef1e2623de250b8d2724d064
Kondrat Shmoylov <kondrat.shmoylov@gmail.com>
2026-04-13 12:17:08 +0200

    temp commit

--- lightly_studio_view/src/lib/components/SampleDetails/SampleDetailsNavigation/SampleDetailsNavigation.svelte
+++ lightly_studio_view/src/lib/components/SampleDetails/SampleDetailsNavigation/SampleDetailsNavigation.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import { page } from '$app/state';
-    import { goto } from '$app/navigation';
+    import { goto, preloadData } from '$app/navigation';
     import { routeHelpers } from '$lib/routes';
     import SteppingNavigation from '$lib/components/SteppingNavigation/SteppingNavigation.svelte';
     import { useAnnotationLabelContext } from '$lib/contexts/SampleDetailsAnnotation.svelte';
@@ -17,45 +17,66 @@
         })
     );
 
-    const gotoNextSample = () => {
+    const nextURL = $derived.by(() => {
         if ($sampleAdjacentQuery.data?.next_sample_id) {
-            goto(
-                routeHelpers.toSample({
-                    sampleId: $sampleAdjacentQuery.data?.next_sample_id,
-                    datasetId,
-                    collectionType,
-                    collectionId: collectionId
-                }),
-                {
-                    invalidateAll: true
-                }
-            );
+            return routeHelpers.toSample({
+                sampleId: $sampleAdjacentQuery.data?.next_sample_id,
+                datasetId,
+                collectionType,
+                collectionId: collectionId
+            });
+        }
+        return null;
+    });
+
+    const gotoNextSample = () => {
+        if (nextURL) {
+            goto(nextURL, {
+                invalidateAll: true
+            });
         }
     };
 
-    const gotoPreviousSample = () => {
+    const prevURL = $derived.by(() => {
         if ($sampleAdjacentQuery.data?.previous_sample_id) {
-            goto(
-                routeHelpers.toSample({
-                    sampleId: $sampleAdjacentQuery.data?.previous_sample_id,
-                    datasetId,
-                    collectionType,
-                    collectionId: collectionId
-                }),
-                {
-                    invalidateAll: true
-                }
-            );
+            return routeHelpers.toSample({
+                sampleId: $sampleAdjacentQuery.data?.previous_sample_id,
+                datasetId,
+                collectionType,
+                collectionId: collectionId
+            });
+        }
+        return null;
+    });
+
+    const gotoPreviousSample = () => {
+        if (prevURL) {
+            goto(prevURL, {
+                invalidateAll: true
+            });
         }
     };
 
     const { context } = useAnnotationLabelContext();
+    const hasNext = $derived(!!$sampleAdjacentQuery.data?.next_sample_id);
+    const hasPrevious = $derived(!!$sampleAdjacentQuery.data?.previous_sample_id);
+
+    $effect(() => {
+        if (nextURL) {
+            console.log('Preloading next sample:', nextURL);
+            preloadData(nextURL);
+        }
+        if (prevURL) {
+            preloadData(prevURL);
+            console.log('Preloading previous sample:', prevURL);
+        }
+    });
 </script>
 
 {#if $sampleAdjacentQuery.data}
     <SteppingNavigation
-        hasPrevious={!!$sampleAdjacentQuery.data?.previous_sample_id}
-        hasNext={!!$sampleAdjacentQuery.data?.next_sample_id}
+        {hasPrevious}
+        {hasNext}
         onPrevious={gotoPreviousSample}
         onNext={gotoNextSample}
         isDrawing={context?.isDrawing}
