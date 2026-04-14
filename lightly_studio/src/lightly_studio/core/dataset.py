1f6a05bab6bd64c9becae8abb44319b2bf4622d3
Michal Szabados <michal@lightly.ai>
2026-04-14 11:01:24 +0200

    Fix docstring

--- lightly_studio/src/lightly_studio/core/dataset.py
+++ lightly_studio/src/lightly_studio/core/dataset.py
@@ -218,8 +218,9 @@ class Dataset(Generic[T], ABC):
         dataset.
 
         Args:
-            sample_metadata: List of (sample ID, metadata_map) tuples. `metadata_map` is a
-            mapping from string to any type, e.g. `{"weather": "cloudy", "temperature": 25}`.
+            sample_metadata: List of `(sample ID, metadata_map)` tuples, where
+                `metadata_map` is a mapping from string to any type, for example
+                `{"weather": "cloudy", "temperature": 25}`.
 
         Example:
             ```python
