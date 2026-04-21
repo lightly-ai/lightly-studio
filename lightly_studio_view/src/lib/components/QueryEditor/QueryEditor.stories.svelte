bfc4c5d4e04ca2a8c6238b3950c7e4258aea07f3
Kondrat Shmoylov <kondrat.shmoylov@gmail.com>
2026-04-21 17:14:22 +0200

    feat: introduce QueryEditor component

---
+++ lightly_studio_view/src/lib/components/QueryEditor/QueryEditor.stories.svelte
@@ -0,0 +1,38 @@
+<script module>
+    import { defineMeta } from '@storybook/addon-svelte-csf';
+    import QueryEditor from './QueryEditor.svelte';
+
+    const { Story } = defineMeta({
+        title: 'Components/QueryEditor',
+        component: QueryEditor,
+        tags: ['autodocs'],
+        argTypes: {
+            value: { control: 'text' },
+            height: { control: 'text' },
+            readOnly: { control: 'boolean' }
+        }
+    });
+
+    const defaultValue = `# Lightly query examples
+ImageSampleField.width > 1920
+ImageSampleField.tags.contains("reviewed")
+VideoSampleField.duration > 60
+`;
+</script>
+
+<Story
+    name="Default"
+    args={{
+        value: defaultValue,
+        height: '360px'
+    }}
+/>
+
+<Story
+    name="Read only"
+    args={{
+        value: defaultValue,
+        height: '240px',
+        readOnly: true
+    }}
+/>
