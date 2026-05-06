2ed545a6a00c83a186099835de031fdbb83afa17
Horatiu Almasan <horatiu@lightly.ai>
2026-05-06 17:20:43 +0300

    fix

--- lightly_studio/src/lightly_studio/resolvers/evaluation_sample_metric_resolver/__init__.py
+++ lightly_studio/src/lightly_studio/resolvers/evaluation_sample_metric_resolver/__init__.py
@@ -1,5 +1,9 @@
-from .create_many import create_many
-from .get_all_by_evaluation_run_id import get_all_by_evaluation_run_id
+from lightly_studio.resolvers.evaluation_sample_metric_resolver.create_many import (
+    create_many,
+)
+from lightly_studio.resolvers.evaluation_sample_metric_resolver.get_all_by_evaluation_run_id import (  # noqa: E501
+    get_all_by_evaluation_run_id,
+)
 
 __all__ = [
     "create_many",
