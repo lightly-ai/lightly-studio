dd2cf9e80b22c5a9a5ad8f4afc5227ebb3ffb5c5
Kondrat Shmoylov <kondrat.shmoylov@gmail.com>
2026-05-11 11:50:38 +0200

    define types and use index

--- lightly_studio_view/src/lib/hooks/useAnnotationCollections/useAnnotationCollections.ts
+++ lightly_studio_view/src/lib/hooks/useAnnotationCollections/useAnnotationCollections.ts
@@ -1,7 +1,14 @@
 import { readAnnotationCollectionsOptions } from '$lib/api/lightly_studio_local/@tanstack/svelte-query.gen';
-import { createQuery } from '@tanstack/svelte-query';
+import type { AnnotationCollectionView } from '$lib/api/lightly_studio_local/types.gen';
+import { createQuery, type CreateQueryResult } from '@tanstack/svelte-query';
 
-export const useAnnotationCollections = ({ collectionId }: { collectionId: string }) => {
+interface UseAnnotationCollectionsParams {
+    collectionId: string;
+}
+
+export const useAnnotationCollections = ({
+    collectionId
+}: UseAnnotationCollectionsParams): CreateQueryResult<AnnotationCollectionView[], Error> => {
     return createQuery(
         readAnnotationCollectionsOptions({
             path: { collection_id: collectionId }
