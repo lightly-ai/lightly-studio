2be4063da8a3d0a0b444609f6bce0cfc2316a714
Lukáš Poláček <lukas@lightly.ai>
2026-05-12 13:59:56 +0200

    Do not overflow docs tooltips
    Especially for first line words, they appeared above the word and were
    thus hidden.

--- lightly_studio_view/src/lib/components/QueryEditor/useQueryEditor.ts
+++ lightly_studio_view/src/lib/components/QueryEditor/useQueryEditor.ts
@@ -64,7 +64,8 @@ export const useQueryEditor = () => {
             automaticLayout: true,
             readOnly: options.readOnly ?? false,
             minimap: { enabled: false },
-            wordWrap: 'on'
+            wordWrap: 'on',
+            fixedOverflowWidgets: true
         });
 
         const detachLanguage = language.attach(model);
