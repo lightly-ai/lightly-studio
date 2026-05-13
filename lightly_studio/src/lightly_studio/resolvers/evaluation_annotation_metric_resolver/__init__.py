f47052feb587d6ce7bdc1a09a771db365b7ab9f5
Horatiu Almasan <horatiu@lightly.ai>
2026-05-13 16:35:16 +0300

    simplify

--- lightly_studio/src/lightly_studio/resolvers/evaluation_annotation_metric_resolver/__init__.py
+++ lightly_studio/src/lightly_studio/resolvers/evaluation_annotation_metric_resolver/__init__.py
@@ -1,11 +1,7 @@
 from lightly_studio.resolvers.evaluation_annotation_metric_resolver.create_many import (
     create_many,
 )
-from lightly_studio.resolvers.evaluation_annotation_metric_resolver.get_all_by_evaluation_run_id import (  # noqa: E501
-    get_all_by_evaluation_run_id,
-)
 
 __all__ = [
     "create_many",
-    "get_all_by_evaluation_run_id",
 ]
