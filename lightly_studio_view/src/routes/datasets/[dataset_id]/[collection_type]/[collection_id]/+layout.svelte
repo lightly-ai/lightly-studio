d64ad587610ca3cc5447d9bffcc6937a0f377f3e
LeonardoRosaa <leonardoorosaa16@gmail.com>
2026-05-22 14:53:49 -0300

    fix nullable

--- lightly_studio_view/src/routes/datasets/[dataset_id]/[collection_type]/[collection_id]/+layout.svelte
+++ lightly_studio_view/src/routes/datasets/[dataset_id]/[collection_type]/[collection_id]/+layout.svelte
@@ -375,7 +375,7 @@
                         {canSelectAll}
                         {isImages}
                         {hasMediaWithEmbeddings}
-                        collectionDatasetId={collection?.dataset_id}
+                        collectionDatasetId={collection.dataset_id}
                         compact={isSidePanelOpen}
                         onSelectAll={selectAllHandle.handleSelectAll}
                         searchImage={$searchImage}
@@ -441,7 +441,7 @@
                                 {canSelectAll}
                                 {isImages}
                                 {hasMediaWithEmbeddings}
-                                collectionDatasetId={collection?.dataset_id}
+                                collectionDatasetId={collection.dataset_id}
                                 compact={isSidePanelOpen}
                                 onSelectAll={selectAllHandle.handleSelectAll}
                                 searchImage={$searchImage}
