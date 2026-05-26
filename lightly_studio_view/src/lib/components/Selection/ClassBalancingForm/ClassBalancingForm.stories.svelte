53ce2c92d45c567d9ca41dbe441865f80770f636
LeonardoRosaa <leonardoorosaa16@gmail.com>
2026-05-26 12:43:39 -0300

    add class balancing form

---
+++ lightly_studio_view/src/lib/components/Selection/ClassBalancingForm/ClassBalancingForm.stories.svelte
@@ -0,0 +1,25 @@
+<script module>
+    import { defineMeta } from '@storybook/addon-svelte-csf';
+    import { fn } from 'storybook/test';
+    import ClassBalancingForm from './ClassBalancingForm.svelte';
+
+    const { Story } = defineMeta({
+        title: 'Components/Selection/ClassBalancingForm',
+        component: ClassBalancingForm,
+        tags: ['autodocs'],
+        argTypes: {
+            balancingMode: {
+                control: 'select',
+                options: ['uniform', 'input']
+            }
+        }
+    });
+</script>
+
+<Story
+    name="Default"
+    args={{
+        balancingMode: 'uniform',
+        onBalancingModeChange: fn()
+    }}
+/>
