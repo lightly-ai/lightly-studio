2d709cae3f4561179d3ec3d71419d2689d6235ca
LeonardoRosaa <leonardoorosaa16@gmail.com>
2026-06-01 13:09:26 -0300

    add StrengthField component for combination sampling

---
+++ lightly_studio_view/src/lib/components/Sampling/forms/StrengthField/StrengthField.stories.svelte
@@ -0,0 +1,34 @@
+<script module>
+    import { defineMeta } from '@storybook/addon-svelte-csf';
+    import { fn } from 'storybook/test';
+    import StrengthField from './StrengthField.svelte';
+
+    const { Story } = defineMeta({
+        title: 'Components/Sampling/StrengthField',
+        component: StrengthField,
+        tags: ['autodocs'],
+        argTypes: {
+            strength: { control: 'number' }
+        }
+    });
+</script>
+
+<Story
+    name="Default"
+    args={{
+        strength: 1,
+        id: 'strength-default',
+        testid: 'strength-field-default',
+        onUpdate: fn()
+    }}
+/>
+
+<Story
+    name="HighStrength"
+    args={{
+        strength: 5,
+        id: 'strength-high',
+        testid: 'strength-field-high',
+        onUpdate: fn()
+    }}
+/>
