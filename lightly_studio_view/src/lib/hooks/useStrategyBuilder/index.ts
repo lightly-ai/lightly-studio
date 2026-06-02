ed434bc67c4a0c664662d80fbed76935d14219b7
LeonardoRosaa <leonardoorosaa16@gmail.com>
2026-06-02 07:28:53 -0300

    add similarity form for combination sampling

--- lightly_studio_view/src/lib/hooks/useStrategyBuilder/index.ts
+++ lightly_studio_view/src/lib/hooks/useStrategyBuilder/index.ts
@@ -7,6 +7,8 @@ export {
     type StrategyParams,
     type StrategyInstance,
     STRATEGY_DEFAULTS,
-    STRATEGY_LABELS
+    STRATEGY_LABELS,
+    type SimilarityParams,
+    type StrategySummaryTag
 } from './types';
 export { cloneStrategyParams, createStrategyInstance } from './strategyInstance';
