c601f48ceda3158fb02cb1aa977323e06b477c01
Kondrat Shmoylov <kondrat@lightly.ai>
2026-06-03 15:59:23 +0200

    Add lang="ts" to the story’s <script module> tag

--- lightly_studio_view/src/lib/components/Select/Select.stories.svelte
+++ lightly_studio_view/src/lib/components/Select/Select.stories.svelte
@@ -1,4 +1,4 @@
-<script module>
+<script module lang="ts">
     import { defineMeta } from '@storybook/addon-svelte-csf';
     import { fn } from 'storybook/test';
     import { Crosshair, Plus, Sigma, Target, TrendingUp } from '@lucide/svelte';
