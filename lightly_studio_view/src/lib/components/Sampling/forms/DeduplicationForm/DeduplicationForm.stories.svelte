844a222390976ab943b157dc40a004800660b249
Horatiu Almasan <horatiu@lightly.ai>
2026-06-18 14:51:26 +0300

    add storybook

---
+++ lightly_studio_view/src/lib/components/Sampling/forms/DeduplicationForm/DeduplicationForm.stories.svelte
@@ -0,0 +1,22 @@
+<script module>
+    import { defineMeta } from '@storybook/addon-svelte-csf';
+    import { fn } from 'storybook/test';
+    import DeduplicationForm from './DeduplicationForm.svelte';
+
+    const { Story } = defineMeta({
+        title: 'Components/Sampling/DeduplicationForm',
+        component: DeduplicationForm,
+        tags: ['autodocs'],
+        argTypes: {
+            params: { control: 'object' }
+        }
+    });
+</script>
+
+<Story
+    name="Default"
+    args={{
+        params: { strength: 1, stopping_condition_minimum_distance: 0.1 },
+        onUpdate: fn()
+    }}
+/>
