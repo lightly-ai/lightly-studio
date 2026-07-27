fa6c06a19fc76fad97e6308a25738f8710c8e92f
Leonardo Rosa <43169851+LeonardoRosaa@users.noreply.github.com>
2026-07-27 15:28:40 -0300

    Analytics: track export_dialog_opened and export_dialog_dismissed (#1773)

--- lightly_studio_view/src/lib/hooks/index.ts
+++ lightly_studio_view/src/lib/hooks/index.ts
@@ -44,6 +44,7 @@ export { useSubmitCombinationSelection } from '$lib/hooks/useSubmitCombinationSe
 export { useOperatorsDialog } from '$lib/hooks/useOperatorsDialog/useOperatorsDialog';
 export { useDeleteAnnotation } from '$lib/hooks/useDeleteAnnotation/useDeleteAnnotation';
 export { usePostHog } from '$lib/hooks/usePostHog';
+export { useExportDialog } from '$lib/hooks/useExportDialog/useExportDialog';
 export { useSettings } from '$lib/hooks/useSettings';
 export { useTrackSampleInspected } from '$lib/hooks/useTrackSampleInspected';
 export { useAnnotationClassVisibility } from '$lib/hooks/useAnnotationClassVisibility/useAnnotationClassVisibility';
