e34bb9dc47ad1569417bba4346ae878f9c737259
ikondrat <kondrat.shmoylov@gmail.com>
2026-08-18 18:01:54 +0200

    update

--- lightly_studio/src/lightly_studio/resolvers/image_resolver/annotation_count_helpers/__init__.py
+++ lightly_studio/src/lightly_studio/resolvers/image_resolver/annotation_count_helpers/__init__.py
@@ -3,11 +3,13 @@ from .build_grouped_count_query import build_grouped_count_query
 from .build_sample_tag_counts import build_sample_tag_counts
 from .get_and_validate_sample_tags import get_and_validate_sample_tags
 from .get_annotation_collection_ids import get_annotation_collection_ids
+from .restrict_to_annotation_sources import restrict_to_annotation_sources
 
 __all__ = [
     "build_count_expression",
     "build_grouped_count_query",
     "build_sample_tag_counts",
     "get_and_validate_sample_tags",
-    "get_annotation_collection_ids"
+    "get_annotation_collection_ids",
+    "restrict_to_annotation_sources"
 ]
