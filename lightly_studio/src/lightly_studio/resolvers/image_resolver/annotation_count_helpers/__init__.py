ba14df5435c2dbcd5aee12f07a3b629ecf1344aa
ikondrat <kondrat.shmoylov@gmail.com>
2026-08-18 17:55:06 +0200

    update

--- lightly_studio/src/lightly_studio/resolvers/image_resolver/annotation_count_helpers/__init__.py
+++ lightly_studio/src/lightly_studio/resolvers/image_resolver/annotation_count_helpers/__init__.py
@@ -1,5 +1,7 @@
 from .build_count_expression import build_count_expression
+from .build_grouped_count_query import build_grouped_count_query
 
 __all__ = [
     "build_count_expression",
+    "build_grouped_count_query"
 ]
