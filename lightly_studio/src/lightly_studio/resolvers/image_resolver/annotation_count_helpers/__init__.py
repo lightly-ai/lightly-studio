1b45f9962fbe71d5d362fada47549226721fbbea
Kondrat Shmoylov <kondrat.shmoylov@gmail.com>
2026-08-19 14:39:57 +0000

    restrict to annotation sources (#2006)

--- lightly_studio/src/lightly_studio/resolvers/image_resolver/annotation_count_helpers/__init__.py
+++ lightly_studio/src/lightly_studio/resolvers/image_resolver/annotation_count_helpers/__init__.py
@@ -3,6 +3,7 @@ from .build_grouped_count_query import build_grouped_count_query
 from .build_sample_tag_counts import build_sample_tag_counts
 from .get_and_validate_sample_tags import get_and_validate_sample_tags
 from .get_annotation_collection_ids import get_annotation_collection_ids
+from .restrict_to_annotation_sources import restrict_to_annotation_sources
 
 __all__ = [
     "build_count_expression",
@@ -10,4 +11,5 @@ __all__ = [
     "build_sample_tag_counts",
     "get_and_validate_sample_tags",
     "get_annotation_collection_ids",
+    "restrict_to_annotation_sources",
 ]
