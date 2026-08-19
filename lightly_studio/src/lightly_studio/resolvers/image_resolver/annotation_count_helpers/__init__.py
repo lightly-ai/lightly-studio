7bc09af358460f6add648be38daaafe5ebad913c
Kondrat Shmoylov <kondrat.shmoylov@gmail.com>
2026-08-19 13:59:19 +0000

    add get_annotation_collection_ids (#2005)

--- lightly_studio/src/lightly_studio/resolvers/image_resolver/annotation_count_helpers/__init__.py
+++ lightly_studio/src/lightly_studio/resolvers/image_resolver/annotation_count_helpers/__init__.py
@@ -2,10 +2,12 @@ from .build_count_expression import build_count_expression
 from .build_grouped_count_query import build_grouped_count_query
 from .build_sample_tag_counts import build_sample_tag_counts
 from .get_and_validate_sample_tags import get_and_validate_sample_tags
+from .get_annotation_collection_ids import get_annotation_collection_ids
 
 __all__ = [
     "build_count_expression",
     "build_grouped_count_query",
     "build_sample_tag_counts",
     "get_and_validate_sample_tags",
+    "get_annotation_collection_ids",
 ]
