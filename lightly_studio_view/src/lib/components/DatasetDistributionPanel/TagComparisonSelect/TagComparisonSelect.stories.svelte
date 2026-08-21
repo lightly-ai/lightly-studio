179bb40aae3cead58abbc32f09d86448b21d894d
ikondrat <kondrat.shmoylov@gmail.com>
2026-08-21 11:32:22 +0200

    fix: mark story module as TypeScript

--- lightly_studio_view/src/lib/components/DatasetDistributionPanel/TagComparisonSelect/TagComparisonSelect.stories.svelte
+++ lightly_studio_view/src/lib/components/DatasetDistributionPanel/TagComparisonSelect/TagComparisonSelect.stories.svelte
@@ -1,4 +1,4 @@
-<script module>
+<script module lang="ts">
     import { defineMeta } from '@storybook/addon-svelte-csf';
     import TagComparisonSelect from './TagComparisonSelect.svelte';
     import { MultiSelectList } from '$lib/components/MultiSelectList';
