bf0e229398972960e13b5d3f63efb1f96fcbd45b
LeonardoRosaa <leonardoorosaa16@gmail.com>
2026-09-14 08:58:00 -0300

    avoid propagation

--- lightly_studio_view/src/lib/components/PointCloudLabelingWorkspace/CuboidLayer/CuboidVisual.svelte
+++ lightly_studio_view/src/lib/components/PointCloudLabelingWorkspace/CuboidLayer/CuboidVisual.svelte
@@ -45,9 +45,9 @@
 />
 <T.Mesh
     visible={false}
-    onclick={onClick}
-    onpointerenter={() => onhover?.(item.annotation.id, null)}
-    onpointerleave={() => onhover?.(null, null)}
+    onclick={(event) => { event.stopPropagation(); onClick(); }}
+    onpointerenter={(event) => { event.stopPropagation(); onhover?.(item.annotation.id, null); }}
+    onpointerleave={(event) => { event.stopPropagation(); onhover?.(null, null); }}
 >
     <T.BoxGeometry args={[...item.annotation.size]} />
     <T.MeshBasicMaterial />
