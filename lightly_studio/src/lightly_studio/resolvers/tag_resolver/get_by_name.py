359c6104db929e46d9fcc493f0ddff818db49640
Lukáš Poláček <lukas@lightly.ai>
2026-09-16 10:14:22 +0200

    Implement review comments

--- lightly_studio/src/lightly_studio/resolvers/tag_resolver/get_by_name.py
+++ lightly_studio/src/lightly_studio/resolvers/tag_resolver/get_by_name.py
@@ -12,10 +12,11 @@ from lightly_studio.models.tag import TagTable
 def get_by_name(
     session: Session,
     tag_name: str,
-    collection_id: UUID | None,
+    collection_id: UUID,
 ) -> TagTable | None:
-    """Retrieve a single tag by name within an optional collection."""
-    query = select(TagTable).where(TagTable.name == tag_name)
-    if collection_id:
-        query = query.where(TagTable.collection_id == collection_id)
+    """Retrieve a single tag by name within a collection."""
+    query = select(TagTable).where(
+        TagTable.collection_id == collection_id,
+        TagTable.name == tag_name,
+    )
     return session.exec(query).one_or_none()
