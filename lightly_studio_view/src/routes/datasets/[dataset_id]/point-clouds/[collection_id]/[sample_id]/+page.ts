40d9bbd5e8f5aa8031df16e4e015d09e26be5de8
ikondrat <kondrat.shmoylov@gmail.com>
2026-09-23 17:19:37 +0200

    Render initial summary

--- lightly_studio_view/src/routes/datasets/[dataset_id]/point-clouds/[collection_id]/[sample_id]/+page.ts
+++ lightly_studio_view/src/routes/datasets/[dataset_id]/point-clouds/[collection_id]/[sample_id]/+page.ts
@@ -1,12 +1,17 @@
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = async ({ params, url }) => {
+    const sequenceId = url.searchParams.get('sequence_id') ?? undefined;
+
+    if (!sequenceId) {
+        throw new Error('Either sequence_id or collection_id must be provided in the URL.');
+    }
     return {
         datasetId: params.dataset_id,
         collectionType: url.searchParams.get('collection_type') ?? undefined,
         collectionId: params.collection_id,
         sampleId: params.sample_id,
-        sequenceId: url.searchParams.get('sequence_id') ?? undefined,
+        sequenceId,
         groupId: url.searchParams.get('group_id') ?? undefined
     };
 };
