f0083ce8a1edb091b16ca9c80bd5d0052dda0d19
IgorSusmelj <igor_7779@hotmail.com>
2026-09-25 09:36:41 +0200

    Add changes

---
+++ lightly_studio_view/src/lib/components/SampleDetails/SampleSlicRect/SlicResultHarness.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import { useSlicResult } from './useSlicResult.svelte';
+    let {
+        imageUrl,
+        level = 'medium',
+        color = 'blue'
+    }: ReturnType<Parameters<typeof useSlicResult>[0]> = $props();
+    const slic = useSlicResult(() => ({ imageUrl, level, color }));
+</script>
+
+<p role="status">{slic.status}</p>
+{#if slic.result}
+    <img alt="Superpixel boundaries" src={slic.boundaryDataUrl} />
+{/if}
